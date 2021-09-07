e1afa503810d0cac967de720b2146ad2fda3272b
ullmer <ullmer@acm.org>
2021-09-07 09:19:23 -0400

    Stub

---
+++ 2021/02.matplotlib/plotEx01.py
@@ -0,0 +1,8 @@
+#Progressive intersections of example Matplotlib/pyplot code and COVID datasets
+#Brygg Ullmer, Clemson University
+#Begun 2021-09-07
+#https://matplotlib.org/stable/tutorials/introductory/pyplot.html#sphx-glr-tutorials-introductory-pyplot-py
+
+
+### end ###
+
