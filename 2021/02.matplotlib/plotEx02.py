4b12667bdbe240a51b779e6fc7c8b7582dd9c3ff
ullmer <ullmer@acm.org>
2021-09-07 09:38:24 -0400

    Simplifying

--- 2021/02.matplotlib/plotEx02.py
+++ 2021/02.matplotlib/plotEx02.py
@@ -12,6 +12,11 @@ plt.figure(figsize=(9, 3))
 
 plt.subplot(131)
 plt.bar(names, values)
+plt.subplot(132)
+plt.scatter(names, values)
+plt.subplot(133)
+plt.plot(names, values)
+plt.suptitle('Categorical Plotting')
 plt.show()
 
 ### end ###
