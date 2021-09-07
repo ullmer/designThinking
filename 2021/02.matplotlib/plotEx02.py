7f73ee91e946123f7e43d6790322ca2e5d379a76
ullmer <ullmer@acm.org>
2021-09-07 09:53:30 -0400

    Beginning to integrate SC hospitals covid data

---
+++ 2021/02.matplotlib/plotEx02.py
@@ -0,0 +1,23 @@
+#Progressive intersections of example Matplotlib/pyplot code and COVID datasets
+#Brygg Ullmer, Clemson University
+#Begun 2021-09-07
+#https://matplotlib.org/stable/tutorials/introductory/pyplot.html#sphx-glr-tutorials-introductory-pyplot-py
+
+import csv, sys, traceback, matplotlib.pyplot as plt
+
+covidF        = open('procHospital9.csv')
+dataReader    = csv.reader(covidF, delimiter=',');
+
+names = []; values = []
+for row in dataReader:
+  try:
+    state, city, date, d1, d2, d3, d4 = row
+    names.append(city); values.append(d1)
+
+plt.figure()
+#plt.subplot()
+plt.bar(names, values)
+plt.show()
+
+### end ###
+
