08fe35f47c78b09c07b0cc81df90949a0145e6f7
ullmer <ullmer@acm.org>
2021-09-07 09:04:13 -0400

    Initial code and derived data

---
+++ 2021/01.csv/procHospital7.py
@@ -0,0 +1,21 @@
+#Clemson Design Thinking, 2021-08-30 (Brygg Ullmer)
+import csv, sys, traceback
+covidF        = open('COVID-19_Reported_Patient_Impact_and_Hospital_Capacity_by_Facility.csv', 'r+t')
+dataReader    = csv.reader(covidF, delimiter=','); 
+
+def getFieldsFloat(d, fields): # extract specified float fields from list d
+  result = []
+  for field in fields: result.append(float(d[field]))
+  return result
+
+def perc(f): return str(int(100*f))
+
+for d in dataReader:
+  try:
+    city, state, date                        = d[6], d[2], d[1]
+    tac, tb, taa, icuBeds, icuUsed, icuCovid = getFieldsFloat(d, [35, 31, 34, 21, 24, 25])
+    covidBeds = perc(tac/tb); ocuBeds=perc(taa/tb); percIcu = perc(icuUsed/icuBeds); pIC = perc(icuCovid/icuBeds)
+    if float(percIcu) >= 200: print(','.join([state, city, date, covidBeds, ocuBeds, pIC, percIcu]))
+  except: pass #traceback.print_exc()
+
+### end ###
