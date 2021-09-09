afe4846d411d9e96ba7b222549c1af532cede26b
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-09 08:45:23 -0400

    yaml progress

---
+++ 2021/03.yaml/procYaml01.py
@@ -0,0 +1,9 @@
+# YAML working examples
+# Brygg Ullmer, Clemson University, 2021-09-09
+
+import yaml
+
+yfn   = 'hospital-fields-pediatric.yaml'; yf = open(yfn, 'r')
+ydata = yaml.load(yf)
+
+### end ###
