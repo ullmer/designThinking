a0161559d5e7b7f94d3231ea37f5e6abb3317a82
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-14 08:29:39 -0400

    refactoring

--- 2021/03.yaml/procHospital10.py
+++ 2021/03.yaml/procHospital10.py
@@ -1,5 +1,5 @@
 #Clemson Design Thinking, 2021-08-30 (Brygg Ullmer)
-import yaml, csv, sys, traceback, dtHelper
+import yaml, csv, sys, traceback, dt
 
 try:    yamlF         = open(sys.argv[0], 'r')
 except: print("problem opening command-line YAML metainfo; aborting"); sys.exit(-1)
@@ -12,11 +12,11 @@ dataReader    = csv.reader(covidF, delimiter=',');
 
 for cd in dataReader: #cd = CSV Data
   try:
-    city, state, date = getFieldsText(cd, yd, ['city', 'state', 'date'])
+    city, state, date = dt.getFieldsText(cd, yd, ['city', 'state', 'date'])
     tac, tb, taa, icuBeds, icuUsed, icuCovid =
-      getFieldsFloat(cd, yd, ['tac', 'tb', 'taa', 'icuBeds', 'icuUsed', 'icuCovid'])
+      dt.getFieldsFloat(cd, yd, ['tac', 'tb', 'taa', 'icuBeds', 'icuUsed', 'icuCovid'])
     covidBeds = perc(tac/tb); ocuBeds=perc(taa/tb); percIcu = perc(icuUsed/icuBeds); pIC = perc(icuCovid/icuBeds)
-    if float(covidBeds) >= 20: print(','.join([state, city, date, covidBeds, ocuBeds, pIC, percIcu]))
+    if float(covidBeds) >= 20: printCSVLine print(','.join([state, city, date, covidBeds, ocuBeds, pIC, percIcu]))
   except: pass #traceback.print_exc()
 
 ### end ###
