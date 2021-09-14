26977bb77178fcdd7c1cab9dde55754ae097be55
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-14 08:36:08 -0400

    fleshing out helper functions

--- 2021/03.yaml/dt.py
+++ 2021/03.yaml/dt.py
@@ -2,13 +2,19 @@
 import yaml, csv, sys, traceback, dtHelper
 
 ################## extract data fields ##################
+# extract specified fields from list cd (short for csvData), using 
+# fields with fieldnames specified in yd (yamlData)
 
-def getFieldsText(d, fields): # extract specified float fields from list d
+def getFieldsText(cd, yd, fields): 
   result = []
-  for field in fields: result.append(d[field])
+  for field in fields: 
+    if field in yd:
+      idx   = yd[field]
+      value = cd[idx]
+      result.append(value)
   return result
 
-def getFieldsFloat(cd, yd, fields): # extract specified float fields from list d
+def getFieldsFloat(cd, yd, fields): # extract specified float fields from list cd
   tresult = getFieldsText(cd, yd, fields)
   for field in fields: result.append(float(d[field]))
   return result
