c6efee3bd8d9870b7565864cd3f35d1b6529745b
ullmer <ullmer@acm.org>
2021-09-14 07:20:03 -0400

    splitting off helper functions

--- 2021/03.yaml/procHospital10.py
+++ 2021/03.yaml/procHospital10.py
@@ -8,22 +8,6 @@ yd = yaml.safeload(yamlF) #yd = YAML data
 covidF        = open(yd['sourceDataCsv'], 'r+t')
 dataReader    = csv.reader(covidF, delimiter=','); 
 
-################## extract data fields ##################
-
-def getFieldsText(d, fields): # extract specified float fields from list d
-  result = []
-  for field in fields: result.append(d[field])
-  return result
-
-def getFieldsFloat(cd, yd, fields): # extract specified float fields from list d
-  tresult = getFieldsText(cd, yd, fields)
-  for field in fields: result.append(float(d[field]))
-  return result
-
-################## extract percentages ##################
-
-def perc(f): return str(int(100*f))
-
 ################## main ##################
 
 for cd in dataReader: #cd = CSV Data
