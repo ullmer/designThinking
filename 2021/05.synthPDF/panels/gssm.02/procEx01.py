41c134439962968dbeb4aa6dbe752cadd16fe4cb
ullmer <ullmer@acm.org>
2021-09-15 16:45:37 -0400

    python stub

---
+++ 2021/05.synthPDF/panels/gssm.02/procEx01.py
@@ -0,0 +1,10 @@
+# Processing of panel metadata: imbibe .yaml, express .pdf
+# By Brygg Ullmer and Aika Washington, Clemson University
+# Begun 2021-09-15
+
+import yaml
+from reportlab.pdfgen import canvas
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+
+### end ###
