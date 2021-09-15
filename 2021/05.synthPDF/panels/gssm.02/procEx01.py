622674c186e7866eb0fed982ff655231f945251a
ullmer <ullmer@acm.org>
2021-09-15 16:49:06 -0400

    python stub

--- 2021/05.synthPDF/panels/gssm.02/procEx01.py
+++ 2021/05.synthPDF/panels/gssm.02/procEx01.py
@@ -3,8 +3,13 @@
 # Begun 2021-09-15
 
 import yaml
-from reportlab.pdfgen import canvas
-from PyPDF2 import PdfFileWriter, PdfFileReader
+#from reportlab.pdfgen import canvas
+#from PyPDF2 import PdfFileWriter, PdfFileReader
 
+sourceYFn = "descriptives.yaml"
+sourceYF  = open(sourceYFn, "r+t")
+sourceYD  = yaml.safe_load(sourceYF)
+
+print(sourceYD)
 
 ### end ###
