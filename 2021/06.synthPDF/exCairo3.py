3352790761c0b9f5a4af6bedd7ba183f522ead83
Brygg Ullmer <ullmer@acm.org>
2021-09-22 10:11:57 -0400

    image+text integration progress

--- 2021/06.synthPDF/exCairo3.py
+++ 2021/06.synthPDF/exCairo3.py
@@ -3,20 +3,19 @@
 # Begun 2021-09-22
 
 # https://zetcode.com/gfx/pycairo/backends/
-# https://stackoverflow.com/questions/7099630/create-pdf-with-resized-png-images-using-pycairo-rescaling-surface-issue
+# https://zetcode.com/gfx/pycairo/images/
 
 import cairo
 from PIL import Image
 
 def main():
 
-    imgFn = 'images/computing2.png'
-    imgF  = open(imgFn, 'r+b')
-    
-    ps = cairo.PDFSurface("exCairo2.pdf", 504, 648)
+    imgFn   = 'images/computing2.png'
+    imgSurf = cairo.ImageSurface.create_from_png(imgFn)
+
+    ps = cairo.PDFSurface("exCairo3.pdf", 504, 648)
     cr = cairo.Context(ps)
 
-    
     cr.set_source_rgb(0, 0, 0)
     cr.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
         cairo.FONT_WEIGHT_NORMAL)
@@ -24,6 +23,10 @@ def main():
     
     cr.move_to(10, 50)
     cr.show_text("Forward ho.")
+
+    cr.set_source_surface(imgSurf, 10, 10)
+    cr.paint()
+
     cr.show_page()
         
         
