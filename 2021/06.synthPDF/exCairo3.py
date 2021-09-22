33ec64e958b897a7e0c17b37da1c6333ade14346
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-22 09:55:32 -0400

    working to add images

---
+++ 2021/06.synthPDF/exCairo3.py
@@ -0,0 +1,29 @@
+# Integration of text and image examples, rendering to PDF
+# Brygg Ullmer, Clemson University
+# Begun 2021-09-22
+
+# https://zetcode.com/gfx/pycairo/backends/
+# https://stackoverflow.com/questions/7099630/create-pdf-with-resized-png-images-using-pycairo-rescaling-surface-issue
+
+import cairo
+from PIL import Image
+
+def main():
+    
+    ps = cairo.PDFSurface("exCairo2.pdf", 504, 648)
+    cr = cairo.Context(ps)
+    
+    cr.set_source_rgb(0, 0, 0)
+    cr.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
+        cairo.FONT_WEIGHT_NORMAL)
+    cr.set_font_size(40)
+    
+    cr.move_to(10, 50)
+    cr.show_text("Forward ho.")
+    cr.show_page()
+        
+        
+if __name__ == "__main__":    
+    main()
+
+### end ###
