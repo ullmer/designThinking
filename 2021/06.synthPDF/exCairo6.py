4c6a5496c50a9837e3aa4eed0c88c8768b3d261f
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-22 17:57:57 -0400

    scaling progress

---
+++ 2021/06.synthPDF/exCairo6.py
@@ -0,0 +1,54 @@
+# Integration of text and image examples, rendering to PDF
+# Brygg Ullmer, Clemson University
+# Begun 2021-09-22
+
+# https://zetcode.com/gfx/pycairo/backends/
+# https://zetcode.com/gfx/pycairo/images/
+
+from PIL import Image
+import cairo
+import socDb
+
+def name2image(name):
+  lowerName = name.lower()
+  name2     = lowerName.replace(' ', '_')
+  result    = "images/soc/%s.png" % name2
+  return result
+
+def main():
+
+  soc = socDb.socDb()
+  hccFaculty = soc.getFacultyByDivision('HCC')
+
+  print("hccFaculty:", str(hccFaculty))
+
+  ps = cairo.PDFSurface("exCairo5.pd6", 504, 648)
+  cr = cairo.Context(ps)
+
+  cr.set_source_rgb(0, 0, 0)
+  #cr.select_font_face("Futura Bk BT")
+  cr.select_font_face("Georgia", cairo.FONT_SLANT_NORMAL,
+                                 cairo.FONT_WEIGHT_NORMAL)
+  cr.set_font_size(24)
+
+  xImg = 10;  xText = 30
+  ypos = 150; yx = 30  
+  for faculty in hccFaculty:
+    cr.move_to(xText, ypos)
+    cr.show_text(faculty); ypos += yx
+
+    imageFn = name2image(faculty)
+    try:
+      imgSurf  = cairo.ImageSurface.create_from_png(imageFn)
+      imgSurf2 = cairo.ImageSurface.image_surface_create_for_data(imgSurf, CAIRO_FORMAT_RGB24, 100, 100, 0)
+      cr.set_source_surface(imgSurf, xImg, ypos)
+      cr.paint()
+    except: print("ignoring image:", imageFn)
+
+  cr.show_page()
+      
+      
+if __name__ == "__main__":    
+  main()
+
+### end ###
