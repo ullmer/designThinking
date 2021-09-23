d20b773a0c9373fd649c04614bf16c5cba7b00bb
Brygg Ullmer <bullmer@clemson.edu>
2021-09-23 00:32:56 -0400

    extraRoles addenda

--- 2021/06.synthPDF/extraRoles.sql
+++ 2021/06.synthPDF/extraRoles.sql
@@ -3,3 +3,11 @@ update faculty set extraRole='CS Chair' where lastName='Dean';
 update faculty set extraRole='VC Chair' where lastName='Zordan';
 update faculty set extraRole='HCC Chair' where lastName='Ullmer';
 update faculty set extraRole='FOI Chair' where lastName='Goddard';
+update faculty set extraRole='Assoc. Director' where lastName='Smotherman';
+update faculty set extraRole='Admin Lead' where lastName='Babb';
+
+update faculty set extraRole='HCC Ph.D. Coordinator' where lastName='Kraemer';
+update faculty set extraRole='CS Ph.D. Coordinator' where lastName='Sorber';
+update faculty set extraRole='Undergrad Coordinator' where lastName='Plaue';
+update faculty set extraRole='DPA Director' where lastName='Patterson';
+
