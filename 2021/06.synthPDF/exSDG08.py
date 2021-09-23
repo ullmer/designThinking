1f3582043d3090050202eac7673742f18a4a02ab
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-23 08:51:32 -0400

    reformatting for 12 per page instead of 6

---
+++ 2021/06.synthPDF/exSDG08.py
@@ -0,0 +1,53 @@
+# Integration of text and image examples, rendering to PDF
+# Brygg Ullmer, Clemson University
+# Begun 2021-09-22
+
+# https://zetcode.com/gfx/pycairo/backends/
+# https://zetcode.com/gfx/pycairo/images/
+# https://zetcode.com/gfx/pycairo/shapesfills/
+# https://zetcode.com/gfx/pycairo/transparency/
+# https://pycairo.readthedocs.io/en/latest/reference/surfaces.html
+
+from PIL import Image
+import cairo, traceback
+
+def genSdgImgFn(sdgNum): return "images/unsdg/%0.2i.png" % sdgNum
+
+
+def main():
+  pageWidth = 72*7; pageHeight = 72*5 # 5x7" pages
+  ps = cairo.PDFSurface("exSDG08.pdf", pageWidth, pageHeight)
+  cr = cairo.Context(ps)
+  s = .035
+  cr.scale(s,s)
+  cr.select_font_face("Arial", cairo.FONT_SLANT_NORMAL,
+                               cairo.FONT_WEIGHT_BOLD)
+  xImgOrig = xImg = 500; dxImg = 3000
+  yImgOrig = yImg = 1000; dyImg = 3000
+  pageNum = 1
+
+  for sdgNum in range(1,18):
+    imageFn = genSdgImgFn(sdgNum)
+
+    if sdgNum % 12 == 1:
+      cr.set_source_rgba(0.8, 0.6, 0, .95)
+      cr.rectangle(0, 0, pageWidth*40, 1200)
+      cr.fill()
+
+      cr.set_font_size(800); cr.move_to(100,700); cr.set_source_rgba(1,1,1,.7)
+      cr.show_text('un sdg :: p%i' % pageNum); pageNum += 1
+
+    try:
+      imgSurf  = cairo.ImageSurface.create_from_png(imageFn)
+      cr.set_source_surface(imgSurf, xImg, yImg)
+      cr.paint()
+    except: print("ignoring image:", imageFn); traceback.print_exc()
+
+    if    (sdgNum - 1) % 3 == 0 and (sdgNum - 1) % 12 != 0: xImg += dxImg;  yImg = yImgOrig
+    else: yImg += dyImg
+    if    sdgNum % 12 == 0: cr.show_page(); xImg = xImgOrig # render + new page
+        
+if __name__ == "__main__":    
+  main()
+  
+### end ###
