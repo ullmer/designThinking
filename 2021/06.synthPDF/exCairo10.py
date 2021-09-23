d03696aff5923a20d6e0e037a7b8b9341823e3d1
Brygg Ullmer <bullmer@clemson.edu>
2021-09-23 00:43:29 -0400

    mostly integrated extra role

--- 2021/06.synthPDF/exCairo10.py
+++ 2021/06.synthPDF/exCairo10.py
@@ -24,7 +24,7 @@ def main():
   xImg = 10; xTxt =  315
   rankMap = {'asst':'Asst. Prof.', 'assoc':'Assoc. Prof.', 'full':'Professor'}
 
-  ps = cairo.PDFSurface("exCairo10.pdf", 3000, 648)
+  ps = cairo.PDFSurface("exCairo10.pdf", 2300, 648)
   cr = cairo.Context(ps)
   s = .38
   cr.scale(s,s)
@@ -48,6 +48,8 @@ def main():
 
     idx = 0; 
     for faculty in divFaculty:
+      if idx != 0 and idx % 6 == 0:  yTxt = yTxtOrig; xTxt += 600
+
       name, rank, extraRole = faculty
       if idx % 1 == 0: cr.set_source_rgba(0.8, 0.6, 0, .1)
       else:            cr.set_source_rgba(0.8, 0.6, 0, .95)
@@ -77,10 +79,10 @@ def main():
         cr.show_text(extraRole)
   
       yTxt += dyTxt;    idx += 1
-      if idx % 5 == 0:  yTxt = yTxtOrig; xTxt += 600
   
     idx = 0
     for faculty in divFaculty:
+      if idx != 0 and idx % 6 == 0: yImg = yImgOrig; xImg += 600
       name, rank, extra = faculty
       imageFn = name2image(name)
       try:
@@ -91,7 +93,6 @@ def main():
   
       idx += 1; yImg += dyImg
   
-      if idx % 5 == 0: yImg = yImgOrig; xImg += 600
 
     xImg += 600; xTxt +=  600
   
