9681e12ccbc551661959e7e089811789be18c614
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-27 09:09:31 -0400

    change in font, width

--- 2021/07.2Dui/exTki04.py
+++ 2021/07.2Dui/exTki04.py
@@ -8,12 +8,15 @@ soc = socDb()
 def helloCB():
   print("hello was pushed")
 
-root      = Tk() 
-divisions = soc.getDivisions()
-colWidth  = 30
+root       = Tk() 
+divisions  = soc.getDivisions()
+colWidth   = 20
+headerFont = ('Sans','12','bold')
+bodyFont   = ('Sans','12')
 
 for division in divisions:
-  w    = Button(root, text=division, command=helloCB, width=colWidth)
+  w    = Button(root, text=division, command=helloCB, 
+                width=colWidth, font=headerFont)
   w.pack(side=LEFT)
 
 root.mainloop()                                          
