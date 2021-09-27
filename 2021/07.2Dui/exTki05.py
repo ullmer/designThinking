81d2469392a1835ebfda2e84a1aeffe9368fdc54
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-27 09:54:12 -0400

    binding event functionality with partial command

---
+++ 2021/07.2Dui/exTki05.py
@@ -0,0 +1,33 @@
+# https://en.wikipedia.org/wiki/Tkinter
+
+from tkinter   import *
+from socDb     import *
+from functools import partial
+
+soc = socDb()
+
+def divisionCb(whichCategory):
+  print("division %s was pushed" % whichCategory)
+
+root       = Tk() 
+divisions  = soc.getDivisions()
+colWidth   = 20
+headerFont = ('Sans','12','bold')
+bodyFont   = ('Sans','12')
+
+#https://www.geeksforgeeks.org/how-to-pass-arguments-to-tkinter-button-command/
+#https://stackoverflow.com/questions/15331726/how-does-functools-partial-do-what-it-does
+#https://stackoverflow.com/questions/2297336/tkinter-specifying-arguments-for-a-function-thats-called-when-you-press-a-butt
+
+for division in divisions:
+  w    = Button(root, text=division, command=partial(divisionCb,division),
+                width=colWidth, font=headerFont)
+
+  #w    = Button(root, text=division, command=lambda: divisionCb(division),
+  w.pack(side=LEFT)
+
+root.mainloop()                                          
+
+#getFacultyByDivision
+
+### end ###
