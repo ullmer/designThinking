4ee2e32b7695c7feb6be03ef1aa67b6371cd86a2
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-09-29 08:40:33 -0400

    refactoring database expression

--- 2021/07b.gui/socDb.py
+++ 2021/07b.gui/socDb.py
@@ -9,7 +9,7 @@
 # and SQL tables described in: soc-defs.sql
 # ... and is synthesized by Python3 code procSoC1.py 
 
-import sqlite3
+import sqlite3, yaml
 
 try: import pandas as pd # this has particular value for Jupyter use
 except: print("pandas not found; working around")
@@ -19,6 +19,8 @@ except: print("pandas not found; working around")
 
 class socDb:
   sqliteDbFn = 'soc.db3'
+  queriesYFn = 'soc-queries.yaml'
+  queriesY   = None
   dbConn     = None
   dbCursor   = None
   verbose    = False
@@ -29,6 +31,16 @@ class socDb:
   def __init__(self):
     self.dbConn   = sqlite3.connect(self.sqliteDbFn)
     self.dbCursor = self.dbConn.cursor()
+    self.loadYamlQueries(self.queriesYFn)
+
+############### load YAML queries ###############
+
+  def loadYamlQueries(self, yamlFn):
+    yf       = open(yamlFn, "r+t")
+    queriesY = yaml.safe_load(yf)
+    yf.close()
+
+    print(queriesY)
 
 ############### show major research areas ###############
 
