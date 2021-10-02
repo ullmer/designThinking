3a8f040bec9373b6f6a0bffe5f159214205d0324
ullmer <ullmer@acm.org>
2021-10-02 09:42:37 -0400

    beginning Enodia Shapefile abstractions

--- 2021/08.roads/enShapefile.py
+++ 2021/08.roads/enShapefile.py
@@ -27,7 +27,8 @@ class enShapefile:
   numRecs = None
 
   targetRoads    = [10,40,80,90] #Interstates
-  targetRoadStrs = []
+  targetRoadStrs = None
+  roadVertexSeqs = None
 
   ################ constructor ################ 
 
@@ -39,51 +40,51 @@ class enShapefile:
    self.fields  = sf.fields
    self.records = sf.records()
 
+   self.targetRoadStrs = []
+   self.roadVertexSeqs = {}
+
    self.extractInterstateVerts()
    self.calcLatLongMinMaxRange()
 
+  ################ extract Interstate Vertices ################ 
 
+  method extractInterstateVerts(self):
+    for tr in self.targetRoads:
+      trStr = "I- " + str(tr)
+      self.targetRoadStrs.append(trStr)
 
+    for i in range(self.numRecs):
+      sl = len(self.shapes[i].points) #shape length (relative to points)
+      name = self.records[i][1]       #road name
 
+      if (len(name.rstrip()) > 0 and name[0]=='I' and name[1]=='-'):
+        if name in self.targetRoadStrs:
+          #print("shape %i : points %i : name %s" % (i, sl, name))
+          numPoints = len(shapes[i].points)
 
-for tr in targetRoads:
-  trStr = "I- " + str(tr)
-  targetRoadStrs.append(trStr)
+          if name not in self.roadVertexSeqs.keys(): 
+	    self.roadVertexSeqs[name] = []
 
+          for coord in self.shapes[i].points: 
+	    self.roadVertexSeqs[name].append(coord)
+    
+  ############ calculate latitude, longitude min, max, range ############
 
+   method calcLatLongMinMaxRange(self):
 
-
-
-for i in range(numRecs):
-  sl = len(shapes[i].points)
-  name = records[i][1]
-  if (len(name.rstrip()) > 0 and name[0]=='I' and name[1]=='-'):
-    if name in targetRoadStrs:
-      #print("shape %i : points %i : name %s" % (i, sl, name))
-      numPoints = len(shapes[i].points)
-      if name not in roadVertexSeqs.keys(): roadVertexSeqs[name] = []
-      for coord in shapes[i].points:        roadVertexSeqs[name].append(coord)
-
-
-   self.calcLatLongMinMaxRange()
-### get minimum and maximum ###
-
-latMin = latMax = longMin = longMax = None
-
-for rvs in roadVertexSeqs.keys():
-  for vertex in roadVertexSeqs[rvs]:
-    lat, long = vertex
-    if latMin == None: latMin = latMax = lat; longMin = longMax = long
-    else:
-      if lat < latMin: latMin = lat
-      if lat > latMax: latMax = lat
-
-      if long < longMin: longMin = long
-      if long > longMax: longMax = long
-
-
-latRange  = abs(latMax - latMin)
-longRange = abs(longMax - longMin)
-
-
+    for rvs in self.roadVertexSeqs.keys():
+      for vertex in roadVertexSeqs[rvs]:
+        lat, long = vertex
+        if latMin == None: latMin = latMax = lat; longMin = longMax = long
+        else:
+          if lat < latMin: latMin = lat
+          if lat > latMax: latMax = lat
+    
+          if long < longMin: longMin = long
+          if long > longMax: longMax = long
+    
+    
+    latRange  = abs(latMax - latMin)
+    longRange = abs(longMax - longMin)
+    
 ### end ###
