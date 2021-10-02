75bb20e59c9c9197c9b3a8f00f6e38558bf85ab0
ullmer <ullmer@acm.org>
2021-10-02 13:51:18 -0400

    adding more interstates; about to replace manual spec as loop

--- 2021/08.roads/exShape08.py
+++ 2021/08.roads/exShape08.py
@@ -5,7 +5,15 @@
 from enShapefile import *
 
 es       = enShapefile()
-es.targetRoads = [10, 20, 30, 40, 50, 60, 70, 80, 90,  5, 15, 25, 35, 45, 55, 65, 75, 85, 95]
+es.targetRoads = []
+
+for i in range(10, 91, 10): es.targetRoads.append(i)
+for i in range( 5, 96, 10): es.targetRoads.append(i)
+print('targetRoads:', es.targetRoads)
+
+#es.targetRoads = [10, 20, 30, 40, 50, 60, 70, 80, 90,  5, 15, 25, 35, 45, 55, 65, 75, 85, 95]
+#es.targetRoads = [10, 90,  5, 95]
+
 es.extractInterstateVerts()
 es.calcLatLongMinMaxRange()
 
