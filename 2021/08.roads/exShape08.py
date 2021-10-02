32736a3faf321de8188b318835a8b94d830f2a00
ullmer <ullmer@acm.org>
2021-10-02 13:03:06 -0400

    pulling out from enShapefile main

---
+++ 2021/08.roads/exShape08.py
@@ -0,0 +1,20 @@
+# Enodia Shapefile abstractions
+# Brygg Ullmer, Clemson University
+# Begun 10/02/2021
+
+import enShapefile
+
+es       = enShapefile()
+targetRoads    = [10,40,80,90] #Interstates
+rvs      = es.roadVertexSeqs
+rvsNames = rvs.keys()
+
+es.plotCaiCreateSurface()
+
+for rvsName in rvsNames:      #primary road names
+  for rvSeq in rvs[rvsName]: #list of constituitive vertex sequences
+    es.plotCaiVertSeq(rvSeq)
+
+es.plotCaiWritePng()
+    
+### end ###
