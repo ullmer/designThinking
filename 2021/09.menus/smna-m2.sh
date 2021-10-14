f79843d0ee47a1abb885d2e3a3a239a293007f7d
ullmer <ullmer@acm.org>
2021-10-14 01:04:51 -0400

    html to yaml progress

--- 2021/09.menus/smna-m2.sh
+++ 2021/09.menus/smna-m2.sh
@@ -1,3 +1,4 @@
 sed -n '320,1062 p' < menus.asp | egrep '^<h3>|^<th>|^<p>          &nbsp;' > m1
-cat m1 | sed 's/^<h3>/ - /;s/<\/h3>$//' > m2
+cat m1 | sed 's/^<h3>/ - /;s/<\/h3>$//'              > m2
 cat m2 | sed 's/^<th><cite>/   - /; s/<\/cite>.*$//' > m3
+cat m3 | sed 's/^<p>.*nbsp;/      - /; s/<\/p>//;'   > m4
