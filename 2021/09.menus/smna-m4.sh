be0376314cfbd34df7172980f6b259f0e64c9e19
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-10-14 08:27:49 -0400

    breaking apart first step

---
+++ 2021/09.menus/smna-m4.sh
@@ -0,0 +1,9 @@
+mkdir tmp
+sed -n '320,1062 p' < menus.asp                             > tmp/m0
+cat tmp/m0 | egrep '^<h3>|^<th>|^<p> *&nbsp;'               > tmp/m1
+cat tmp/m1 | sed 's/^<h3>/ - /;s/<\/h3>$//'                 > tmp/m2
+cat tmp/m2 | sed 's/^<th><cite>/   - {main: /'              > tmp/m3
+cat tmp/m3 | sed 's/<\/cite>&nbsp;/, sides: /; s/<\/th>/}/' > tmp/m4
+cat tmp/m4 | sed 's/^<p>.*nbsp;/      - /; s/<\/p>//;'      > tmp/m5
+cat tmp/m5 | sed 's/\&amp\;/ & /g;'                         > tmp/m6
+
