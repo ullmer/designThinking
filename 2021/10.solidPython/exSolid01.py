ae49664f1237728437439dd76cf787123ff5da62
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-10-27 23:46:00 -0400

    First example

---
+++ 2021/10.solidPython/exSolid01.py
@@ -0,0 +1,13 @@
+# SolidPython example code 
+# Brygg Ullmer, Clemson University
+
+from solid import *        # load in SolidPython/SCAD support code
+
+c1 = cube()
+c2 = translate([1.5, 0, 0])(c1)
+outGeom = c1 + c2
+
+print(scad_render(outGeom))
+
+### end ###
+
