b629590ca6b3a4b1877e02d6cc836bfa33dab357
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-10-28 09:51:41 -0400

    first test

--- 2021/10.solidPython/synthSCnodes1.py
+++ 2021/10.solidPython/synthSCnodes1.py
@@ -4,16 +4,21 @@
 
 from solid import *        # load in SolidPython/SCAD support code
 
-c1 = cube()
-c2 = translate([1.5, 0, 0])(c1)
-outGeom = c1 + c2
+scPopCoordFn = 'sc-pop-coord.csv'; scPCF = open(scPopCoordFn, "r")
+scpcReader   = csv.reader(scPCF, delimiter=",")
 
+outGeom = None
 y1 = cylinder(r=.3, h=.6)
-y2 = translate([.5, .5, .5])(y1)
-outGeom += y2
+
+for row in scpcReader:
+  city, pop, lat, long = row
+  y2 = translate([lat, long, 0])(y1)
+
+  if outGeom == None: outGeom = y2
+  else:               outGeom += y2
 
 radialSegments = 25; hdr = '$fn = %s;' % radialSegments # create a header for the export
-scad_render_to_file(outGeom, 'exSolid02.scad', file_header=hdr) # write the .scad file
+scad_render_to_file(outGeom, 'scNodes1.scad', file_header=hdr) # write the .scad file
 
 ### end ###
 
