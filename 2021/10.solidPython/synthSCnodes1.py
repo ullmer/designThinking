5494775c4e3543aa22dc78f7df6cf3b45c36a22d
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-10-28 09:57:10 -0400

    v1

--- 2021/10.solidPython/synthSCnodes1.py
+++ 2021/10.solidPython/synthSCnodes1.py
@@ -3,16 +3,17 @@
 # Written 2021-10-27
 
 from solid import *        # load in SolidPython/SCAD support code
+import csv
 
 scPopCoordFn = 'sc-pop-coord.csv'; scPCF = open(scPopCoordFn, "r")
 scpcReader   = csv.reader(scPCF, delimiter=",")
 
 outGeom = None
-y1 = cylinder(r=.3, h=.6)
+y1 = cylinder(r=.02, h=.1)
 
 for row in scpcReader:
   city, pop, lat, long = row
-  y2 = translate([lat, long, 0])(y1)
+  y2 = translate([float(lat), float(long), 0])(y1)
 
   if outGeom == None: outGeom = y2
   else:               outGeom += y2
