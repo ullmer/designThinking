7ab0cbdd0debc616d3388a2a44cd350cc6a62b6d
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-10-28 09:05:00 -0400

    progress

---
+++ 2021/10.solidPython/synthSCcities1.py
@@ -0,0 +1,27 @@
+# Join latlong coordinates and population of SC cities
+# Brygg Ullmer, Clemson University
+# Begun 2021-10-28
+
+import csv
+
+coordsFn = "sc_cities.csv"; coordsF = open(coordsFn, "r")
+popFn = "sc-urban-pop.txt"; popF    = open(popFn, "r")
+
+coordReader = csv.reader(coordsF, delimiter=",")
+popReader   = csv.reader(popF,    delimiter="\t")
+
+coordHash = {}; popHash = {}
+
+rowNum = 0
+for row in coordReader:
+  if rowNum<5: print(row)
+  rowNum += 1
+
+print("="*10)
+
+rowNum = 0
+for row in popReader:
+  if rowNum<5: print(row)
+  rowNum += 1
+
+### end ###
