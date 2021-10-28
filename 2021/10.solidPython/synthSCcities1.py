ea0418ece9c215d9b1fb3d1a96726426f1fb1dc5
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-10-28 09:08:37 -0400

    reworked pop.txt to pop.csv

--- 2021/10.solidPython/synthSCcities1.py
+++ 2021/10.solidPython/synthSCcities1.py
@@ -5,10 +5,10 @@
 import csv
 
 coordsFn = "sc_cities.csv"; coordsF = open(coordsFn, "r")
-popFn = "sc-urban-pop.txt"; popF    = open(popFn, "r")
+popFn = "sc-urban-pop.csv"; popF    = open(popFn, "r")
 
 coordReader = csv.reader(coordsF, delimiter=",")
-popReader   = csv.reader(popF,    delimiter="\t")
+popReader   = csv.reader(popF,    delimiter=",")
 
 coordHash = {}; popHash = {}
 
