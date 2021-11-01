242e3cfd9adfc49151fddeca80763386f822581e
ullmer <ullmer@acm.org>
2021-10-31 23:56:30 -0400

    first half-trial

---
+++ 2021/10.solidPython/synthBolts.py
@@ -0,0 +1,12 @@
+# Synthesize bolts within SolidPython
+# Brygg Ullmer, Clemson University
+# Begun 2021-10-31
+
+import mcmBolts
+
+mb = mcmBolt()
+boltspecs = mb.getBoltspecs()
+print(boltspecs)
+
+### end ###
+
