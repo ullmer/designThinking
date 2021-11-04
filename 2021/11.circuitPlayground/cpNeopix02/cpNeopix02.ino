b68c03b513a3d937e16caba20de6fe919d2e6001
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-11-04 09:59:36 -0400

    progress

--- 2021/11.circuitPlayground/cpNeopix02/cpNeopix02.ino
+++ 2021/11.circuitPlayground/cpNeopix02/cpNeopix02.ino
@@ -10,14 +10,14 @@ void setup() {
 void loop() {
   Adafruit_CircuitPlayground cp;
   cp.begin();
-  int orange[3] = {40, 20, 0}, purple[3] = {40, 0, 40};
+  int orange[3] = {60, 24, 0}, purple[3] = {60, 0, 60};
   
   cp.setPixelColor(0, orange[0], orange[1], orange[2]);
   cp.setPixelColor(1, purple[0], purple[1], purple[2]);
   delay(1000); // milliseconds
 
-  cp.setPixelColor(0, orange[0]/4, orange[1]/4, orange[2]/4);
-  cp.setPixelColor(1, purple[0]/4, purple[1]/4, purple[2]/4);
+  cp.setPixelColor(0, orange[0]/3, orange[1]/3, orange[2]/3);
+  cp.setPixelColor(1, purple[0]/3, purple[1]/3, purple[2]/3);
   delay(1000); // milliseconds
 
   cp.setPixelColor(0, 0, 0, 0); 
