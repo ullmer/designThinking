2ff800335729c2b626ef9b750773fcf8ddc76fbf
ullmer <ullmer@acm.org>
2021-12-03 17:56:26 -0500

    first steps toward yaml-extracted assertions

--- 2021/15.prolog/ex03.pl
+++ 2021/15.prolog/ex03.pl
@@ -4,9 +4,7 @@ module(ex01, []).
 :- use_module(library(yaml/util)).
 :- use_module(library(yaml/serializer)).
 
-assertFaculty(X) :- assertz(faculty(X)).
-
 procYaml :-
     read_yaml(file('soc-faculty.yaml'), YAML),
     parse(YAML, PL),
-    assertFaculty(PL.'divisions'.'HCC'.X)).
+    forall(member(X, PL.'divisions'.'HCC'), assertz(faculty(X))).
