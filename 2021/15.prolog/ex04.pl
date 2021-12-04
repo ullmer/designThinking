28d244a8a73bf2aa917c2b3c2443678d7fa27635
ullmer <ullmer@acm.org>
2021-12-03 23:34:15 -0500

    formatting sorted

--- 2021/15.prolog/ex04.pl
+++ 2021/15.prolog/ex04.pl
@@ -8,14 +8,14 @@ module(ex04, []).
 printBarred(Str) :- 
   Bar='===========', format('\n~w ~w ~w\n', [Bar, Str, Bar]).
 
-procDivisions(PL) :- dict_keys(PL.'divisions', Divisions),
-    foreach(member(Division, Divisions), procDivision(PL, Division)).
+printDivisions(PL) :- dict_keys(PL.'divisions', Divisions),
+    foreach(member(Division, Divisions), printDivFaculty(PL, Division)).
 
-procDivision(PL, Division) :- printBarred(Division),
+printDivFaculty(PL, Division) :- printBarred(Division),
    forall(member(Faculty, PL.'divisions'.Division), writeln(Faculty)).
 
 procYaml :-
     read_yaml(file('soc-faculty.yaml'), YAML), parse(YAML, PL),
-    procDivisions(PL).
+    printDivisions(PL).
 
 
