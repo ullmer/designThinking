c84889f4e07029127c186e63e73dedb32a237e1f
ullmer <ullmer@acm.org>
2021-12-04 00:16:00 -0500

    declaring victory for eve

--- 2021/15.prolog/ex06.pl
+++ 2021/15.prolog/ex06.pl
@@ -1,4 +1,4 @@
-module(ex05, []).
+module(ex06, []).
 
 :- use_module(library(yaml/parser)).
 :- use_module(library(yaml/util)).
@@ -14,10 +14,16 @@ assertDivision(PL, Division) :-
 addFaculty(Division, Faculty) :-
   assertz(faculty(Faculty)), assertz(divisionMember(Faculty, Division)).
 
-procYaml :-
-  read_yaml(file('soc-faculty.yaml'), YAML1), 
-   parse(YAML1, PL1), assertDivisions(PL1),
-  read_yaml(file('soc-research-categories.yaml'), YAML2), 
-   parse(YAML2, PL2), assertMajorAreas(PL2).
+assertMajorAreas(PL) :- dict_keys(PL.'majorAreas', MajorAreas),
+ writeln(MajorAreas),
+ forall(member(MajorArea, MajorAreas), writeln(MajorArea)).
+
+procYaml1 :-
+  read_yaml(file('soc-faculty.yaml'), YAML), 
+   parse(YAML, PL), assertDivisions(PL).
+
+procYaml2 :-
+  read_yaml(file('soc-research-categories.yaml'), YAML), 
+   parse(YAML, PL), writeln('foo'), assertMajorAreas(PL).
 
 %%%% end %%%%
