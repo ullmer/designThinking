fb99ad59c4dcb2d17b0971aa05458e8546d5b23c
U-CAMPUS\bullmer <bullmer@BULLMER-10-M.CAMPUS.CU.CLEMSON.EDU>
2021-12-03 23:02:26 -0500

    progress

--- 2021/15.prolog/ex04.pl
+++ 2021/15.prolog/ex04.pl
@@ -3,11 +3,17 @@ module(ex01, []).
 :- use_module(library(yaml/parser)).
 :- use_module(library(yaml/util)).
 :- use_module(library(yaml/serializer)).
+:- use_module(library(dicts)).
+
+procDivisions(PL) :- dict_keys(PL.'divisions', Divisions),
+    foreach(member(Division, Divisions), procDivision(PL, Division)).
+
+procDivision(PL, Division) :- 
+   Bar='===========', writeln(format('%s%s%s', [Bar, Division, Bar])),
+   forall(member(Faculty, PL.'divisions'.Division), writeln(Faculty)).
 
 procYaml :-
-    read_yaml(file('soc-faculty.yaml'), YAML),
-    parse(YAML, PL),
-    forall(  member(Division, PL.'divisions'), 
-      forall(member(Faculty, Division), 
-                      writeln(Faculty))).
+    read_yaml(file('soc-faculty.yaml'), YAML), parse(YAML, PL),
+    procDivisions(PL).
+
 
