a959d26a95bd9ccd6c1502e7c5e1fbaa70c29851
ullmer <ullmer@acm.org>
2021-12-06 01:10:15 -0500

    renamed PL to YAML; debugging

--- 2021/15.prolog/ex07.pl
+++ 2021/15.prolog/ex07.pl
@@ -3,33 +3,38 @@ module(ex06, []).
 :- use_module(library(yaml)).
 :- use_module(library(dicts)).
 
-assertDivisions(PL) :- dict_keys(PL.'divisions', Divisions),
- foreach(member(Division, Divisions), assertDivision(PL, Division)).
+assertDivisions(YAML) :- dict_keys(YAML.'divisions', Divisions),
+ foreach(member(Division, Divisions), assertDivision(YAML, Division)).
 
-assertDivision(PL, Division) :-
- forall(member(Faculty, PL.'divisions'.Division), 
+assertDivision(YAML, Division) :-
+ forall(member(Faculty, YAML.'divisions'.Division), 
    addFaculty(Division, Faculty)).
 
 addFaculty(Division, Faculty) :-
   assertz(faculty(Faculty)), assertz(divisionMember(Faculty, Division)).
 
-assertMajorAreas(PL) :- dict_keys(PL.'researchAreas', MajorAreas),
- forall(member(MajorArea, MajorAreas), assertSpecificAreas(PL, MajorArea)).
+assertMajorAreas(YAML) :- dict_keys(YAML.'researchAreas', MajorAreas),
+ forall(member(MajorArea, MajorAreas), assertSpecificAreas(YAML, MajorArea)).
 
-assertSpecificAreas(PL, MajorArea) :- 
- dict_keys(PL.'researchAreas'.MajorArea, SpecificAreas),
+assertSpecificAreas(YAML, MajorArea) :- 
+ dict_keys(YAML.'researchAreas'.MajorArea, SpecificAreas),
+ assertz(researchArea(MajorArea)),
  forall(member(SpecificArea, SpecificAreas), 
-   format('~w:~w\n', [MajorArea, SpecificArea])).
+   assertSpecificArea(MajorArea, SpecificArea, 
+     YAML.'researchAreas'.MajorArea.SpecificArea]))).
+   
+assertSpecificArea(MajorArea, SpecificArea, PersonList) :-
+  assertz(researchArea(MajorArea, SpecificArea)),
+  forall(member(Person, PersonList), 
+    assertz(researchFocus(Person, MajorArea, SpecificArea))).
 
 procYaml1 :-
-  read_yaml(file('soc-faculty.yaml'), YAML), 
-   parse(YAML, PL), assertDivisions(PL).
+  yaml_read('soc-faculty.yaml', YAML), 
+  assertDivisions(YAML).
 
 procYaml2 :-
   yaml_read('soc-research-categories.yaml', YAML), 
-   %writeln(YAML), 
    assertMajorAreas(YAML).
 
-%  read_yaml(file('soc-research-categories.yaml'), YAML), 
 
 %%%% end %%%%
