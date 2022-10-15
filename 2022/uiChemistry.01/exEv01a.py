6a81d7905db68cf05b3d6cfb30c881b4d8fc8b85
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2022-10-15 11:42:28 -0400

    From github:ullmer/tei-book/domains/chemistry

---
+++ 2022/uiChemistry.01/exEv01a.py
@@ -0,0 +1,11 @@
+# Brygg Ullmer, Miriam Konkel, and Breanna Filipiak
+# Support class for engaging periodic table of the elements
+
+from   evElements import *
+
+ev = evElements()
+ev.buildGui()
+ev.mainloop()
+
+### end ###
+
