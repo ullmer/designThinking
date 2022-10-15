e8a87fffbc2c5e19bb9ccca9ed7880d6fda81d25
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2022-10-15 12:30:43 -0400

    Continuing intersection

---
+++ 2022/uiChemistry.01/procElementDataOsChIntersect2.py
@@ -0,0 +1,23 @@
+# Brygg Ullmer and Miriam Konkel
+# Intersecting periodic table of the elements with OpenStax Chemistry 2e textbook
+# Begun 2022-10-15
+
+from edElements import *
+import yaml
+
+ed       = edElements()
+elements = ed.getElementList()
+
+yfn = 'os-chemistry-2e6.yaml'
+yf  = open(yfn, 'rt')
+y   = yaml.safe_load(y)
+
+sourceFiles = 'osbooks-chemistry-bundle/modules/*/*'
+targetFiles = 'os-chem2e-elements'
+
+for element in elements: 
+  execStr = 'grep -ic %s %s > %s/%s' % (element, sourceFiles, targetFiles, element)
+  print(execStr)
+
+### end ###
+
