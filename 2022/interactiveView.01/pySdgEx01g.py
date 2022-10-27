dcf961d2067bc3c1802b96d02fd80ca661d28e5f
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2022-10-27 15:49:56 -0400

    in-class progress

---
+++ 2022/interactiveView.01/pySdgEx01g.py
@@ -0,0 +1,27 @@
+# Progressive examples toward simple Python interactivity
+# Brygg Ullmer, Clemson University
+# Begun 2022-10-13
+
+# https://docs.python.org/3/library/tkinter.html
+# https://www.geeksforgeeks.org/python-tkinter-scale-widget/
+# https://www.askpython.com/python-modules/tkinter/tkinter-intvar
+
+import tkinter as tk
+from enoIgridTk import *
+
+top  = tk.Tk()
+#tkig = enoIgridTk(top, numButtons=17, imageLabelDir="sdg", useImageLabels=True)
+tkig = enoIgridTk(top, numButtons=3, imageLabelDir="img/as/A", useImageLabels=True)
+
+sdgIdx = tk.IntVar()
+
+def sdgSliderCb(event):
+  global tkig, sdgIdx
+  tkig.buttonCallback(sdgIdx.get())
+
+slider = tk.Scale(top, var=sdgIdx, from_=1, to=18, command=sdgSliderCb, orient=tk.HORIZONTAL)
+slider.pack(expand=1, fill=tk.X)
+
+top.mainloop()
+
+### end ###
