0c73250268f0830826f24e3c9779f7534d0ad70e
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2022-11-01 23:29:31 -0400

    beginning PyGame Zero refactoring

--- 2022/interactiveView.01/enoUiHomelessnessPgz.py
+++ 2022/interactiveView.01/enoUiHomelessnessPgz.py
@@ -2,11 +2,10 @@
 # Begun 2022-11-01
 # Content engaging https://github.com/DataKind-DC/homelessness-service-navigator
 
-import tkinter as tk
-from PIL import Image, ImageTk
+from PIL import Image, ImagePgz
 from enoDomHomelessness import *
 
-class enoUiHomelessnessTk:
+class enoUiHomelessnessPgz:
 
   edh          = None  #enoDomHomelessness
   tkParent     = None  #tk parent
@@ -32,7 +31,7 @@ class enoUiHomelessnessTk:
     for category in categories:
       imgFn1 = self.edh.getImageFn(category)
       imgFn2 = "images/%s.png" % imgFn1
-      img    = ImageTk.PhotoImage(file=imgFn2)
+      img    = ImagePgz.PhotoImage(file=imgFn2)
       self.imageHandles[category] = img #amazing; this line necessary, per atlasologist reference here
       # https://stackoverflow.com/questions/22200003/tkinter-button-not-showing-image
 
@@ -41,10 +40,18 @@ class enoUiHomelessnessTk:
       b.pack(expand=True, anchor="w")
     self.tkFrame.pack()
 
+  ####################### draw #######################
+
+  def buildUI(self, tkParent):
+
+  ####################### draw #######################
+
+  def onMouseDown(self, pos):
+
 ####################### main #######################
 if __name__ == '__main__':
-  top = tk.Tk()
-  enoUiH = enoUiHomelessnessTk(top)
+  top = tk.Pgz()
+  enoUiH = enoUiHomelessnessPgz(top)
   top.mainloop()
 
 ### end ###
