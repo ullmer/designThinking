5c5cf1f1d17e14d292478a1ba24556fff7b36992
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-09-18 14:15:14 -0400

    grid example

--- 2023/hccFundamentals/midi.01/exTkGrid01.py
+++ 2023/hccFundamentals/midi.01/exTkGrid01.py
@@ -2,9 +2,21 @@
 # Brygg Ullmer, Clemson University
 # Begun 2023-09-18
 
-import tkinter
+import tkinter as tk
 
 rows, columns = 8, 8
+
+mf = mainFrame = tk.Tk()
+mf.title("Interactive grid example")
+mf.geometry("800x600")
+
+for i in seq(rows):
+  for j in seq(columns):
+    str = "%ix%i" % (i, j)
+    button = tk.Button(mf, text=str)
+    button.grid(row=i, column=j)
+
+mf.mainloop()
 
 ### end ###
 
