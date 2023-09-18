6e07969b1378e52df08acc028b4e2a9b41d4ad7b
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-09-18 14:17:26 -0400

    grid example

--- 2023/hccFundamentals/midi.01/exTkGrid01.py
+++ 2023/hccFundamentals/midi.01/exTkGrid01.py
@@ -2,6 +2,8 @@
 # Brygg Ullmer, Clemson University
 # Begun 2023-09-18
 
+# https://www.activestate.com/resources/quick-reads/how-to-position-widgets-in-tkinter/
+
 import tkinter as tk
 
 rows, columns = 8, 8
@@ -10,8 +12,8 @@ mf = mainFrame = tk.Tk()
 mf.title("Interactive grid example")
 mf.geometry("800x600")
 
-for i in seq(rows):
-  for j in seq(columns):
+for i in range(rows):
+  for j in range(columns):
     str = "%ix%i" % (i, j)
     button = tk.Button(mf, text=str)
     button.grid(row=i, column=j)
