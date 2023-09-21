80af5da776ee1690cfc90bc9e642f8679f84b932
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-09-20 23:20:28 -0400

    simplest animation, now toward periodicity

--- 2023/hccFundamentals/matrixInteract/pgzBounce01.py
+++ 2023/hccFundamentals/matrixInteract/pgzBounce01.py
@@ -2,9 +2,12 @@ WIDTH=600
 HEIGHT=800
 
 def draw(): 
+  screen.clear()
   ring.draw()
 
-ring = Actor("ring01")
-ring.pos = 50, 50
+animateTween = 'accel_decel'
+ring         = Actor("ring01")
+ring.pos     = 50, 50
+animate(ring, pos=(600,600), duration=3., tween=animateTween)
 
 ### end ###
