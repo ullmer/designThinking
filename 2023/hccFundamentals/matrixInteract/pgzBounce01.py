24d15f137f6c11332885bd1312825d5dcd004344
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-09-20 23:31:40 -0400

    working well on laptop, testing on hyperpixel

--- 2023/hccFundamentals/matrixInteract/pgzBounce01.py
+++ 2023/hccFundamentals/matrixInteract/pgzBounce01.py
@@ -1,3 +1,9 @@
+# Simple example of bouncing ring
+# Brygg Ullmer, Clemson University
+# Begun 2023-09-20
+
+import sys
+
 WIDTH=600
 HEIGHT=800
 
@@ -5,9 +11,26 @@ def draw():
   screen.clear()
   ring.draw()
 
+endpoints       = [(50, 50), (600, 600)]
+currentEndpoint = 1
+
 animateTween = 'accel_decel'
 ring         = Actor("ring01")
-ring.pos     = 50, 50
-animate(ring, pos=(600,600), duration=3., tween=animateTween)
+ring.pos     = endpoints[0]
+
+################### reverse animation ################### 
+
+def reverseAnimation():
+  global endpoints, currentEndpoint, ring
+
+  if currentEndpoint == 1:
+    animate(ring, pos=endpoints[0], duration=1., tween=animateTween); currentEndpoint = 0
+  else:
+    animate(ring, pos=endpoints[1], duration=1., tween=animateTween); currentEndpoint = 1 
+
+################### main ################### 
+
+animate(ring, pos=endpoints[1], duration=1., tween=animateTween)
+clock.schedule_interval(reverseAnimation, 1.)
 
 ### end ###
