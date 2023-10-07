07d051ea7b6fc2300aa6577237519c2f69fbea7c
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-10-07 00:48:42 -0400

    transparent movie playback progres; movies themselves initially kept separately

--- 2023/hccFundamentals/tkinter/exTkvp01.py
+++ 2023/hccFundamentals/tkinter/exTkvp01.py
@@ -5,17 +5,20 @@ from tkVideoPlayer import TkinterVideo
 
 root = tk.Tk()
 root.geometry("1920x1080+0+0")
-root.attributes('-alpha',0.65)
+#root.attributes('-alpha',0.65)
 
-#fn = 'test2.mp4'
+fn = 'test2.mp4'
 #fn = 'i.mov'
 #fn = 'out01.mov'
-fn = 'out03.mkv'
+#fn = 'out03.mkv'
+
+#def loop(e): videoplayer.play()
 
 #videoplayer = TkinterVideo(master=root, scaled=True)
 videoplayer = TkinterVideo(master=root)
 videoplayer.load(fn)
 videoplayer.pack(expand=True, fill="both")
+#videoplayer.bind("<<Ended>>", loop)
 
 videoplayer.play() # play the video
 
