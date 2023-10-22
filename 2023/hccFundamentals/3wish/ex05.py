ac136c49bea18bfda536c1748eab840fb28ccfaa
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-10-22 11:09:28 -0400

    simplifying

---
+++ 2023/hccFundamentals/3wish/ex05.py
@@ -0,0 +1,61 @@
+# First validation of Python port of 1995 3wish code
+# Brygg Ullmer, Clemson University
+# Original code begun fall 1995; here, 2023-10-20
+
+import FreeCAD as App
+import FreeCADGui as Gui
+import pivy.coin as coin
+import sys
+
+sys.path.append('c:/git/designThinking/2023/hccFundamentals/3wish')
+from w3core  import *
+from w3shift import *
+
+##################### main #####################
+
+if Gui.ActiveDocument is None:
+  doc  = App.newDocument()
+  doc.recompute()
+  viewer = Gui.createViewer()
+  view   = viewer.getViewer()
+else:
+  view = Gui.ActiveDocument.ActiveView
+
+Gui.activeDocument().activeView().setCameraType("Perspective")
+
+sg      = view.getSceneGraph()
+root    = coin.SoSeparator()
+sg.addChild(root)
+
+lts  = coin.SoTransformSeparator() #move light, without partioning from scene (like regular Separator) 
+ltst = coin.SoTranslation(); ltst.translation.setValue(0, .7, 0)
+l1 = coin.SoPointLight()
+l1.color.setValue(1,0,0)
+#l4.intensity.setValue(100)
+lts.addChild(ltst); lts.addChild(l1)
+root.addChild(lts)
+
+s3 = coin.SoSeparator()
+t3 = coin.SoTranslation(); t3.translation.setValue(.7, 0, 0)
+m3 = coin.SoMaterial();    m3.emissiveColor.setValue(80, 0, 0)
+c3 = coin.SoCube()
+for i in [c3.width, c3.height, c3.depth]: i.setValue(.3)
+for i in [t3, m3, c3]: s3.addChild(i)
+root.addChild(s3)
+
+cone1 = coin.SoCone()
+
+s2 = coin.SoSeparator()
+m1 = coin.SoMaterial()
+m1.diffuseColor.setValue(1, 0, 0)
+s2.addChild(m1)
+s2.addChild(cube2)
+
+addNObj(root,  "cone1", cone1)
+#moveNObj(root, "cube2", [3,3,3])
+#moveNObj(root, "cube2", [3,3,3])
+
+Gui.runCommand('Std_ViewZoomOut',0)
+Gui.SendMsgToActiveView("ViewFit")
+
+### end ###
