70dcfa56ffe90683b2802bfce3ae2d397fd90a46
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-10-22 08:48:24 -0400

    lighting experiments

--- 2023/hccFundamentals/3wish/ex04.py
+++ 2023/hccFundamentals/3wish/ex04.py
@@ -21,16 +21,53 @@ if Gui.ActiveDocument is None:
 else:
   view = Gui.ActiveDocument.ActiveView
 
+Gui.activeDocument().activeView().setCameraType("Perspective")
+
 sg      = view.getSceneGraph()
 root    = coin.SoSeparator()
 sg.addChild(root)
-
 cube1, cube2 = coin.SoCube(), coin.SoCube()
-addNObj(root, "cube1", cube1)
-addNObj(root, "cube2", cube2)
-moveNObj(root, "cube2", [3,3,3])
+
+s2 = coin.SoSeparator()
+m1 = coin.SoMaterial()
+m1.diffuseColor.setValue(1, 0, 0)
+s2.addChild(m1)
+s2.addChild(cube2)
+
+s3 = coin.SoSeparator()
+t3 = coin.SoTranslation(); t3.translation.setValue(1.3, 0, 0)
+m3 = coin.SoMaterial();    m3.emissiveColor.setValue(80, 0, 0)
+c3 = coin.SoCube()
+for i in [c3.width, c3.height, c3.depth]: i.setValue(.3)
+
+for i in [t3, m3, c3]: s3.addChild(i)
+root.addChild(s3)
+
+s4 = coin.SoSeparator()
+l4 = coin.SoPointLight()
+l4.color.setValue(1,0,0)
+l4.intensity.setValue(10)
+l4.location.setValue(0, 1.3, 0)
+s4.addChild(l4); root.addChild(s4)
+
+print(0); addNObj(root, "cube1", cube1)
+print(1); addNObj(root, "cube2", s2)
+print(2); moveNObj(root, "cube2", [3,3,3])
+print(3)
 
 Gui.runCommand('Std_ViewZoomOut',0)
 Gui.SendMsgToActiveView("ViewFit")
+
+output = coin.SoOutput()
+wa     = coin.SoWriteAction(output)
+result = wa.apply(s2)
+
+#bs = output.getBufferSize()
+#print("bs:", bs)
+#b  = output.getBuffer(bs)
+
+print(">>>", dir(output))
+print(">>", output)
+#print(">>", b)
 
 ### end ###
