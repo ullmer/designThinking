43dbc82212da1d4dd1b1117854b5942c96a2b145
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-11-08 14:16:44 -0500

    first steps

--- 2023/hccFundamentals/pgzero/exPgz05.py
+++ 2023/hccFundamentals/pgzero/exPgz05.py
@@ -1,10 +1,14 @@
 a1 = Actor('red-hl-1in-200dpi')
 a2 = Actor('red-hl-1in-200dpi', pos=(180, 180))
 
-actors = [a1, a2]
-actorNames = {a1: "John", a2: "Jane"}
+actors         = [a1, a2]
+actorNames     = {a1: "John", a2: "Jane"}
+actorLastPos   = {}
+selectedObjectHandle = None
+selectedObjectName   = None
 
 def draw(): 
+  screen.clear()
   for actor in actors: actor.draw()
 
 def on_mouse_down(pos):
@@ -12,5 +16,17 @@ def on_mouse_down(pos):
     if actor.collidepoint(pos): 
       name = actorNames[actor]
       print("actor selected:", name)
+      actorLastPos[actor] = pos     
+
+      selectedObjectHandle = actor
+      selectedObjectName   = name
+
+  print("=" * 25)
+
+def on_mouse_move(pos):
+  objectName = selectedObjectName
+  lastPos    = actorLastPos[selectObj]
+
+  print("on_mouse_mov:", objectName, lastPos, pos)
 
 ### end ###
