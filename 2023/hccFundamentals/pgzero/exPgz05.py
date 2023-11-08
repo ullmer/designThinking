30eb5256ae9734033aa7f197036ec2a801b7049d
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-11-08 14:35:46 -0500

    first steps

--- 2023/hccFundamentals/pgzero/exPgz05.py
+++ 2023/hccFundamentals/pgzero/exPgz05.py
@@ -3,9 +3,9 @@ import sys
 a1 = Actor('red-hl-1in-200dpi')
 a2 = Actor('red-hl-1in-200dpi', pos=(180, 180))
 
-actors         = [a1, a2]
-actorNames     = {a1: "John", a2: "Jane"}
-actorOriginalPos   = {}
+actors               = [a1, a2]
+actorNames           = {a1: "John", a2: "Jane"}
+actorOriginalPos     = {}
 selectedObjectHandle = None
 selectedObjectName   = None
 
@@ -14,25 +14,22 @@ def draw():
   for actor in actors: actor.draw()
 
 def on_mouse_down(pos):
+  global selectedObjectName, selectedObjectHandle 
   for actor in actors: 
     if actor.collidepoint(pos): 
       name = actorNames[actor]
       print("actor selected:", name)
       actorOriginalPos[actor] = pos     
-
-      selectedObjectHandle = actor
-      selectedObjectName   = name
+      selectedObjectHandle    = actor
+      selectedObjectName      = name
 
   print("=" * 25)
 
 def on_mouse_move(pos):
   print(".", end=''); sys.stdout.flush() # print "." as update, with no newline -- and update
-  objectName = selectedObjectName
   
-  if objectName is not None: #make sure *something* is selected
-    originalPos    = actorOriginalPos[selectObj]
-    print("on_mouse_mov:", objectName, originalPos, pos)
+  if selectedObjectHandle != None: #make sure *something* is selected
+    originalPos  = actorOriginalPos[selectedObjectHandle]
+    print("on_mouse_mov:", selectedObjectName, originalPos, pos)
 
 ### end ###
-
-
