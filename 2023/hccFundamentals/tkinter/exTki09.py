a7d82ff4ae2f9b2d9ae1a26783c0d0cabd281a47
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-11-09 12:17:32 -0500

    removing leftover code from sister pygame zero implementation

--- 2023/hccFundamentals/tkinter/exTki09.py
+++ 2023/hccFundamentals/tkinter/exTki09.py
@@ -19,67 +19,6 @@ selectedActorOrigPos = None
 def helloCB():
   print("hello was pushed")
 
-#screen.draw.circle((800, 500), 50, defaultEllipseColor)
-#
-####################### on mouse down/press ######################
-#
-#def addUser():
-#  newActor = Actor('red-hl-1in-200dpi',  pos=(200, 200))
-#  moveableActors.append(newActor)
-#  actorNames[newActor] = 'new actor'
-#
-####################### on mouse down/press ######################
-#
-#def on_mouse_down(pos):
-#  global selectedActor, selectedActorName, selectedActorOrigPos, stableActors
-#  for actor in (stableActors + moveableActors):
-#    if actor.collidepoint(pos): 
-#      name = actorNames[actor]
-#      print("\nactor selected:", name)
-#
-#      if name == "screen": 
-#        print("update the virtual screen images")
-#        stableActors = [s2, b1]
-#
-#      elif name == "addUser":
-#        addUser()
-#
-#      else:
-#        actorOriginalPos[actor] = pos     
-#        selectedActor           = actor
-#        selectedActorName       = name
-#        selectedActorOrigPos    = selectedActor.pos
-#
-#  print("=" * 25)
-#
-#def on_mouse_move()
-#
-####################### on mouse up ######################
-#
-#def on_mouse_up():
-#  global selectedActor, selectedActorName, selectedActorOrigPos
-#  selectedActor = selectedActorName = selectedActorOrigPos = None
-#
-####################### on key down ######################
-#
-#numTimesSpaceHit = 0
-#
-#def on_key_down(key):
-#  global numTimesSpaceHit
-#
-#  if key == keys.SPACE:  # keys.RIGHT, keys.H, keys.C, etc.
-#    print("space pressed")
-#
-#    #match numTimesSpaceHit:
-#    #  case 0:
-#
-#    if numTimesSpaceHit == 0:
-#      animate(a1, pos=(400, 500), tween='accel_decel', duration=.75)
-#    else:
-#      animate(a2, pos=(500, 500), tween='accel_decel', duration=.75)
-#
-#    numTimesSpaceHit += 1
-#
 ####################### build UI ######################
 
 imP1 = imTk1 = None
