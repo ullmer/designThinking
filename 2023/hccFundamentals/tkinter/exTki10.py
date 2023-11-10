5506d4463f6c969cc07cb2dc80e173026916afb0
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-11-10 09:28:37 -0500

    progress

--- 2023/hccFundamentals/tkinter/exTki10.py
+++ 2023/hccFundamentals/tkinter/exTki10.py
@@ -45,59 +45,56 @@ img1                 = None
 ####################### build user interface ######################
 
 def buildUI(f1Screens, f2Spatial, f3Controls):
-
   imgAddUserFn   = 'person-add-iconic1.png'
-  imP1  = PIL.Image.open(imgAddUserFn)
-  imTk1 = PIL.ImageTk.PhotoImage(imP1)
+  gs.imP1  = PIL.Image.open(imgAddUserFn)
+  gs.imTk1 = PIL.ImageTk.PhotoImage(gs.imP1)
 
   #b = Button(f3Controls, text="add actor", command=helloCB) # Create a label with words
-  b  = Button(f3Controls, image=imTk1, command=addCanvasItem)
+  b  = Button(f3Controls, image=gs.imTk1, command=addCanvasItem)
   b.pack(side=LEFT, expand=True, fill=BOTH) 
 
   screenFilenames = ['unsdg2.png', 'unsdg4.png']
-  imP2   = PIL.Image.open(screenFilenames[0])
-  imTk2  = PIL.ImageTk.PhotoImage(imP2)
-  label1 = Label(f1Screens, image=imTk2)
+  gs.imP2  = PIL.Image.open(screenFilenames[0])
+  gs.imTk2 = PIL.ImageTk.PhotoImage(gs.imP2)
+  label1   = Label(f1Screens, image=gs.imTk2)
   label1.pack()
 
-  bgColor = rgb2tk(10, 10, 10)
+  bgColor  = rgb2tk(10, 10, 10)
   #c = Canvas(f2Spatial, bg="orange", height=200, width=1024)
   c = Canvas(f2Spatial,  bg=bgColor,  height=400, width=1024)
   c.pack()
 
-  img1Fn = 'clemson12d2.png'
-  img1   = PhotoImage(file=img1Fn) #transparent image
-  c.create_image(100, 100, image=img1,anchor='ne') 
+  img1Fn  = 'clemson12d2.png'
+  gs.img1 = PhotoImage(file=img1Fn) #transparent image
+  c.create_image(100, 100, image=gs.img1,anchor='ne') 
 
   r1Coords = (10, 10, 60, 60)
-  r1 = c.create_rectangle(r1Coords, fill="white")
+  r1 = gs.canvas.create_rectangle(r1Coords, fill="white")
 
   r2Coords = (70, 10, 120, 60)
-  r2 = c.create_rectangle(r2Coords, fill="orange")
+  r2 = gs.canvas.create_rectangle(r2Coords, fill="orange")
 
   #print("canvas rectangle ids:", r1, r2)
   #c.move(r1, 50, 50)
 
-  c.bind("<Button-1>",  on_click)
-  c.bind("<B1-Motion>", on_drag)
+  gs.canvas.bind("<Button-1>",  on_click)
+  gs.canvas.bind("<B1-Motion>", on_drag)
 
 ################### mouse click callback ##################
 
 #next pulls from https://stackoverflow.com/questions/65189412/python-canvas-move-items-with-mouse-tkinter
 
-def on_click(event):
-  global c, selectedCanvasObj, lastDragXY
-
+def on_click(gs, event):
   x, y = event.x, event.y
   csr  = 10 #click search radius
   x1, y1, x2, y2 = x-csr, y-csr, x+csr, y+csr
   #print("click event:", event)
 
-  selected = c.find_overlapping(x1, y1, x2, y2)
+  selected = gs.canvas.find_overlapping(x1, y1, x2, y2)
   if selected: selectedCanvasObj = selected[-1]
   else:        selectedCanvasObj = None
 
-  lastDragXY = (x,y) #for calculating dx, dy movement changes with drag
+  gs.lastDragXY = (x,y) #for calculating dx, dy movement changes with drag
 
   #print("selected:", selectedCanvasObj)
 
