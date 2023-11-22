2f8ef3fb0868663ca957448842cb32e9220b5756
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-22 00:24:44 -0500

    Beginning first of several examples

---
+++ 2023/hccFundamentals/nativePeoples/ex01.pl
@@ -0,0 +1,9 @@
+% Example of engaging datasets in this directory
+% Brygg Ullmer, Clemson University
+% Begun 2023-11-22
+
+consult('tribe_entity_mapping_2021-03-04.pl').
+consult('usBea.pl').
+consult('spatial.pl').
+
+%%% end %%%
