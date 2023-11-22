47325f08650b570b54ad201a34a3ba972c4ec564
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-22 17:59:01 -0500

    integrating user agent (per external file not committed to github)

--- 2023/hccFundamentals/nativePeoples/exWp01.py
+++ 2023/hccFundamentals/nativePeoples/exWp01.py
@@ -1,6 +1,16 @@
-import wikipediaapi
+# Wikipedia retrieval example
+# Brygg Ullmer, Clemson University
+# Begun 2023-11-22
 
-ww=wikipediaapi.Wikipedia('en')
+import wikipediaapi
+#https://github.com/martin-majlis/Wikipedia-API
+
+projNameFn = 'wp-projname.txt'; projNameF = open(projNameFn, 'rt')
+projName = projNameFn.readlines[0].rstrip() #first line
+print("Using %s as Wikipedia user-agent" % projName)
+sys.exit(-1)
+
+ww=wikipediaapi.Wikipedia(user_agent=projName, language='en')
 
 wpPagen = "Classification of the Indigenous peoples of the Americas"
 wpPage  = ww.page(wpPagen)
