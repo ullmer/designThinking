6ea8a6c5d99478269f48c5d40d99f950c8a59fda
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-22 00:39:27 -0500

    progress

--- 2023/hccFundamentals/nativePeoples/ex01.pl
+++ 2023/hccFundamentals/nativePeoples/ex01.pl
@@ -6,4 +6,6 @@ consult('tribe_entity_mapping_2021-03-04.pl').
 consult('usBea.pl').
 consult('spatial.pl').
 
+tribeIntersectsRegion(southEast, Name, State).
+
 %%% end %%%
--- 2023/hccFundamentals/nativePeoples/spatial.pl
+++ 2023/hccFundamentals/nativePeoples/spatial.pl
@@ -2,6 +2,8 @@
 % Brygg Ullmer, Clemson University
 % Begun 2023-11-22
 
+:- use_module(library(lists)).
+
 tribeIntersectsState(State, Name) :- 
         tribe(_, _, States, Name), 
             member(State, States).
@@ -10,9 +12,18 @@ tribeIntersectsState(State, Bia, Epa, States, Name) :-
                       tribe(Bia, Epa, States, Name), 
                         member(State, States).
 
-tribeIntersectsRegion(Region, Name) :-
-           tribe(_, _, States1, Name),
-           beaRegion(Region, States2),
-          intersect(States1, States2).
+tribeIntersectsRegion(Region, Name, State) :-
+         tribe(_, _, States1, Name),
+         beaRegion(Region, States2),
+             member(State, States2),
+             member(State, States1).
+
+tribesIntersectRegion(Region, Names) :-
+  findall(Region, tribeIntersectsRegions(Region, Name), Names).
+
+%tribeIntersectsRegion(Region, Name, CommonStates) :-
+%         tribe(_, _, States1, Name),
+%         beaRegion(Region, States2),
+%     intersection(States1, States2, CommonStates).
 
 %%% end %%%
