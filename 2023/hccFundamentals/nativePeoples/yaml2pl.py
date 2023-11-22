a7673a84974162109ad1d7ae2a1c5c9d0592b147
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-21 23:10:16 -0500

    populating

--- 2023/hccFundamentals/nativePeoples/yaml2pl.py
+++ 2023/hccFundamentals/nativePeoples/yaml2pl.py
@@ -9,5 +9,26 @@
 # - {biaCode: 304, epaId: 100000311, states: [MT, ND, SD], name: "Turtle Mountain Band of Chippewa Indians of North Dakota"}
 # - {biaCode: 603, epaId: 100000051, states: [AZ, CA], name: "Colorado River Indian Tribes of the Colorado River Indian Reservation, Arizona and California"}
 
+###################### yaml to prolog ######################
+
+class yaml2pl:
+  yamlFn = None
+  ourPl  = None
+
+  ################### constructor ###################
+
+  def __init__(self, **kwargs): 
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
+
+    self.loadYaml()
+    self.genPl()
+
+###################### main ######################
+
+ourYamlFn = 'tribe_entity_mapping_2021-03-04.yaml'
+ourPlFn   = 'tribe_entity_mapping_2021-03-04.pl'
+
+y2p = yaml2pl(yamlFn=ourYamlFn, plFn=ourPlFn)
 
 ### end ###
