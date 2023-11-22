2f19598b918cde0aeda6083e156fbdeff9ed0399
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-21 22:08:56 -0500

    progress

--- 2023/hccFundamentals/nativePeoples/csv2yaml.py
+++ 2023/hccFundamentals/nativePeoples/csv2yaml.py
@@ -28,7 +28,7 @@ class csv2yaml:
   targetColFields    = [] #keys of targetColDictXC/N
   targetColVals      = [] #vals of targetColDictN
   rowData            = []
-  ignoreRowErrors    = True
+  ignoreRowErrors    = False
   verbose            = False
 
   maxLineNum = 10
@@ -37,12 +37,12 @@ class csv2yaml:
   ################### constructor ###################
 
   def __init__(self, **kwargs): 
-
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
     #https://stackoverflow.com/questions/739625/setattr-with-kwargs-pythonic-or-not
 
     self.mapColId()
     self.loadCsv() #internally checks for csvFn to be populated
+    print("bar")
     self.genYaml() #internally checks for yamlFn to be populated 
   
   ################### map alphabetic to numeric ###################
@@ -57,11 +57,12 @@ class csv2yaml:
   ################### map alphabetic to numeric ###################
   
   def mapColAlpha2Num(self, colAlpha): #map column alphabetic ID (A..Z, AA..AZ, etc.) to numeric
-                                 #initially, hardcode to 1 or 2 alphabetic codes 
+                                       #initially, hardcode to 1 or 2 alphabetic codes 
     numCA  = len(colAlpha)
+
     if numCA == 1: return self.mapAlpha2Num(alpha)
     if numCA > 2:  print('mapColAlpha2Num requires generalization; sorry!'); sys.exit(-1)
-    if numCA == 0: print('mapColAlpha2Num requires 1 or 2 alphabetic characters; sorry!'); sys.exit(-1)
+    if numCA == 0: print('mapColAlpha2Num presently requires 1 or 2 alphabetic characters; sorry!'); sys.exit(-1)
   
     result = 26 * (self.mapAlpha2Num(colAlpha[0])+1) + self.mapAlpha2Num(colAlpha[1]) # hardwired to two 
     return result
@@ -89,11 +90,14 @@ class csv2yaml:
 
       try:
         extractDict = {}
+        numCols = len(row)
         for key in self.targetColDictN:
           colVal  = self.targetColDictN[key]
-          if self.verbose: print(key, colVal)
+          if self.verbose: print(key, colVal); #print(row)
 
-          dataVal = row[colVal]
+          if colVal < numCols: dataVal = row[colVal]
+          else: print("loadCsv line %i issue: colVal %s not present; %s" % (self.lineNum, colVal, str(row))); continue
+
           extractDict[key] = dataVal
 
           valLen  = len(str(dataVal))
@@ -103,7 +107,7 @@ class csv2yaml:
       except:
         if self.ignoreRowErrors: continue
         print("csv2yaml: loadCsv error:")
-        print(traceback.print_exc()); sys.exit(-1)
+        print(traceback.print_exc()); #sys.exit(-1)
 
       if self.verbose: print("%i: %s" % (self.lineNum, str(extractDict)))
       self.rowData.append(extractDict)
@@ -114,20 +118,21 @@ class csv2yaml:
   def genYaml(self): #internally checks for yamlFn to be populated 
     if self.yamlFn is None: print("csv2yaml: genYaml requires yamlFn"); sys.exit(-1)
 
+    print("genYaml called")
+
     for rowDict in self.rowData:
-      rowstr = " - {
+      rowstr = " - {"
       subels = []
 
       for key in self.fieldOrder:
         fieldstr += key + ': '
-          if key in self.quoteFields: fieldstr += '"%s"' % rowDict[key] #initially, ignore justification
-          else:                       fieldstr += rowDict[key]
+        if key in self.quoteFields: fieldstr += '"%s"' % rowDict[key] #initially, ignore justification
+        else:                       fieldstr += rowDict[key]
         subels.append(str)
 
-     rowstr += ','.join(subels)
-     rowstr += '}'
-
-     print(rowstr)
+      rowstr += ','.join(subels)
+      rowstr += '}'
+      print(rowstr)
 
 ################### main ###################
 
