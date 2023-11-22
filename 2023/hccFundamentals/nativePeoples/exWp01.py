9ebfe3e2cd35542b2503f8974970c63c0627755f
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-22 18:05:18 -0500

    initial extraction promising, likely time for bs4

--- 2023/hccFundamentals/nativePeoples/exWp01.py
+++ 2023/hccFundamentals/nativePeoples/exWp01.py
@@ -2,20 +2,26 @@
 # Brygg Ullmer, Clemson University
 # Begun 2023-11-22
 
+import sys, traceback 
 import wikipediaapi
+
 #https://github.com/martin-majlis/Wikipedia-API
 
 projNameFn = 'wp-projname.txt'; projNameF = open(projNameFn, 'rt')
-projName = projNameFn.readlines[0].rstrip() #first line
+projName = projNameF.readlines()[0].rstrip() #first line
 print("Using %s as Wikipedia user-agent" % projName)
-sys.exit(-1)
 
-ww=wikipediaapi.Wikipedia(user_agent=projName, language='en')
+ww=wikipediaapi.Wikipedia(user_agent=projName, language='en', 
+                          extract_format=wikipediaapi.ExtractFormat.HTML)
 
+#wpPagen = "Classification_of_the_Indigenous_peoples_of_the_Americas"
 wpPagen = "Classification of the Indigenous peoples of the Americas"
 wpPage  = ww.page(wpPagen)
 
+links = wpPage.links
+print("Links:", str(links))
+
 wpText = wpPage.text #wpPage.images
-print(wpText)
+print("Text:", wpText)
 
 ### end ###
