a5eb8845c1ea08a1b5e0863d66bd5b151192ead6
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-22 00:19:01 -0500

    US BEA regions

--- 2023/hccFundamentals/nativePeoples/usBea.pl
+++ 2023/hccFundamentals/nativePeoples/usBea.pl
@@ -2,5 +2,18 @@
 % Expressed by Brygg Ullmer, Clemson University
 % Begun 2023-11-22
 
+% https://apps.bea.gov/regional/docs/msalist.cfm?mlist=2
+
+beaRegion(newEngland,     [ct, ma, ms, nh, ri, vt]).
+beaRegion(midEast,        [de, dc, md, nj, ny, pa]).
+beaRegion(greatLakes,         [il, in, mi, oh, wi]).
+beaRegion(plains,     [ia, ks, mn, mi, ne, nd, sd]).
+beaRegion(southEast,  [al, ar, fl, ga, ky, la, ms, 
+                               nc, sc, tn, va, wv]).
+beaRegion(southWest,              [az, nm, ok, tx]).
+
+
+beaRegion(rockyMountain,      [co, id, mo, ut, wy]).
+beaRegion(farWest,        [ak, ca, hi, nv, or, wa]).
 
 %%% end %%%
