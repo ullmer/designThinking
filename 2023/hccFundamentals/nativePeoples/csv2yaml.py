87d0a80959fcfa3e459dcdb5f1df811c640e036a
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-21 20:49:30 -0500

    initial stub

---
+++ 2023/hccFundamentals/nativePeoples/csv2yaml.py
@@ -0,0 +1,8 @@
+# Make partial extraction of CSV-save of tribe_entity_mapping sheet from this source:
+#  https://www.epa.gov/sites/production/files/2021-03/tribe_entity_mapping_2021-03-04.xlsx
+#  into YAML.
+# (Exploratory version to facilitate related conversations.)
+# Brygg Ullmer, Clemson University
+# Begun 2023-11-21
+
+### end ###
