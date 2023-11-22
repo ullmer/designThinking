ef5a617c3e0225708dbd34ed8e6588d0c53b7921
Brygg Ullmer <brygg@Bryggs-Mac-mini.local>
2023-11-21 23:08:01 -0500

    stub for yaml to prolog mapping

---
+++ 2023/hccFundamentals/nativePeoples/yaml2pl.py
@@ -0,0 +1,13 @@
+# Convert YAML listing to Prolog
+# Brygg Ullmer, Clemson University
+# Begun 2023-11-21
+
+# Example YAML text-file representation content:
+# - {biaCode: 604, epaId: 100000091, states: [AZ, CA, NV], name: "Fort Mojave Indian Tribe of Arizona, California & Nevada"}
+# - {biaCode: 803, epaId: 100000092, states: [AZ, NM, OK], name: "Fort Sill Apache Tribe of Oklahoma"}
+# - {biaCode: 780, epaId: 100000171, states: [AZ, NM, UT], name: "Navajo Nation, Arizona, New Mexico, & Utah"}
+# - {biaCode: 304, epaId: 100000311, states: [MT, ND, SD], name: "Turtle Mountain Band of Chippewa Indians of North Dakota"}
+# - {biaCode: 603, epaId: 100000051, states: [AZ, CA], name: "Colorado River Indian Tribes of the Colorado River Indian Reservation, Arizona and California"}
+
+
+### end ###
