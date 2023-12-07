049887c16f482178b9864548b00d096c4694cd92
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2023-12-07 09:31:59 -0500

    with changes

--- 2023/hccFundamentals/midterm-extraCredit/pb-pgz/exPgz08-sd-diff.py
+++ 2023/hccFundamentals/midterm-extraCredit/pb-pgz/exPgz08-sd-diff.py
@@ -2,81 +2,75 @@
 # By Brygg Ullmer, Clemson University
 # Begun 2023-11-08
 
+# Extra credit modification, SD (opted for public):
+# For this activity, I would like to use the Pgz along with my final project.  
+# For now, I will add an image (ascreenshot) from the application prototype,
+# and use the Actors as the input(fingerTouch) by the user
+
 import sys
 
-WIDTH=1024
+WIDTH=1000
 
+class globalState:
+  numTimesSpaceHit  = 0
+  lastSelectedActor = selectedActor = None
+  selectedActorName = None
+  actorOriginalPos  = {}
+  stableActors = moveableActors = actorNames = None
+
+  defaultEllipseLocation = Rect((800, 600), (850, 650))
+
+gs = globalState()
+  
 knownActorFilenames = ['red-hl-1in-200dpi', 'person-iconic1',
                        'ipanel-cell-selection1']
 defaultActorFn      = knownActorFilenames[1]
 
-a1 = Actor(defaultActorFn) #previously: a1 = Actor('red-hl-1in-200dpi')
-a2 = Actor(defaultActorFn,  pos=(180, 180))
-s1 = Actor('unsdg2',             pos=(550, 100)) #H20
-s2 = Actor('unsdg4',             pos=(550, 100)) #NaCl
-b1 = Actor('person-add-iconic1', pos=( 80, 500))
-m1 = Actor('campus-map8',        pos=(348, 202))
-#m1 = Actor('clemson12d2',        pos=(348, 202))
+b1 = Actor('exit',         pos=(925, 75))
+s1 = Actor('sample_screen')
+a3 = Actor('canvas_touch')
 
-successiveScreens = [s1, s2]
-lastSelectedActor = a1
+#s1 = Actor('login_screen', pos=(350, 210))
 
-moveableActors = [m1, a1, a2, b1] # chalraha
-#moveableActors = [a1, a2, b1] # chalraha
-stableActors   = [s1] #achalraha / rukha
-
-actorNames           = {a1: "John", a2: "Jane", s1: "screen", 
-                        b1: "addUser", m1: "map"}
-#                        b1: "addUser"}
-actorOriginalPos     = {}
-selectedActor        = None
-selectedActorName    = None
-selectedActorOrigPos = None
-defaultEllipseColor    = (0, 200, 200)
-defaultEllipseLocation = Rect((800, 600), (850, 650))
+gs.lastSelectedActor = a3
+gs.moveableActors    = [a3]
+gs.stableActors      = [s1, b1]
+gs.actorNames        = {a3: "canvas touch", s1: "login screen", b1: "exit"}
 
 ###################### draw ######################
 
 def draw(): 
   screen.clear()
-  for actor in stableActors:   actor.draw()
-  for actor in moveableActors: actor.draw()
-
-  #placeholder per idea from Yang
-  #pygame.draw.ellipse(screen.surface, defaultEllipseColor, defaultEllipseLocation)
-  screen.draw.circle((800, 500), 50, defaultEllipseColor)
+  for actor in gs.stableActors:   actor.draw()
+  for actor in gs.moveableActors: actor.draw()
 
 ###################### on mouse down/press ######################
 
 def addUser():
   #print("map position:", m1.pos)
-  newActor = Actor('red-hl-1in-200dpi',  pos=(200, 200))
-  moveableActors.append(newActor)
-  actorNames[newActor] = 'new actor'
+  newActor = Actor('red-hl-1in-200dpi', pos=(200, 200))
+  gs.moveableActors.append(newActor)
+  gs.actorNames[newActor] = 'new actor'
 
 ###################### on mouse down/press ######################
 
 def on_mouse_down(pos): # on_press_down
-  global selectedActor, selectedActorName, selectedActorOrigPos
-  global stableActors, lastSelectedActor
-
-  for actor in (stableActors + moveableActors):
+  for actor in (gs.stableActors + gs.moveableActors):
     if actor.collidepoint(pos): 
-      name = actorNames[actor]
+      name = gs.actorNames[actor]
       print("\nactor selected:", name)
 
       if name == "screen": 
         print("update the virtual screen images")
-        stableActors = [s2, b1]
+        gs.stableActors = [s2, b1]
 
-      elif name == "addUser":
-        addUser()
+      elif name == "exit":
+        sys.exit(-1)
 
       else:
-        actorOriginalPos[actor] = pos     
-        lastSelectedActor = selectedActor = actor
-        selectedActorName       = name
-        selectedActorOrigPos    = selectedActor.pos
+        gs.actorOriginalPos[actor] = pos     
+        gs.lastSelectedActor       = gs.selectedActor = actor
+        gs.selectedActorName       = name
 
   print("=" * 25)
 
@@ -85,43 +79,37 @@ def on_mouse_down(pos): # on_press_down
 def on_mouse_move(rel):
   print(".", end=''); sys.stdout.flush() # print "." as update, with no newline -- and update
   
-  if selectedActor != None: #make sure *something* is selected
-    origX, origY = selectedActor.pos
+  if gs.selectedActor != None: #make sure *something* is selected
+    origX, origY = gs.selectedActor.pos
     dx,       dy = rel #relative position; thanks to pg0 magic, we cannot rename that
     newX,   newY = origX+dx, origY+dy
-    selectedActor.pos = (newX, newY)
+    gs.selectedActor.pos = (newX, newY)
 
     #print("on_mouse_mov:", selectedActorName, originalMousePos, pos, dx, dy)
 
 ###################### on mouse up ######################
 
 def on_mouse_up(): #on_press_up
-  global selectedActor, selectedActorName, selectedActorOrigPos
-  lastSelectedActor = selectedActor 
-  selectedActor     = selectedActorName = selectedActorOrigPos = None
+  gs.lastSelectedActor = gs.selectedActor 
+  gs.selectedActor     = gs.selectedActorName = None
 
 ###################### on key down ######################
 
 numTimesSpaceHit = 0
 
 def on_key_down(key):
-  global numTimesSpaceHit, lastSelectedActor
-
   if key == keys.SPACE:  # keys.RIGHT, keys.H, keys.C, etc.
     print("space pressed")
 
-    #match numTimesSpaceHit:
-    #  case 0:
-
     if numTimesSpaceHit == 0:
-      animate(a1, pos=(400, 500), tween='accel_decel', duration=.75)
-    else:
-      animate(a2, pos=(500, 500), tween='accel_decel', duration=.75)
+          animate(a1, pos=(400, 500), tween='accel_decel', duration=.75)
+    else: animate(a2, pos=(500, 500), tween='accel_decel', duration=.75)
 
-    numTimesSpaceHit += 1
+    gs.numTimesSpaceHit += 1
 
-  if key == keys.RIGHT: lastSelectedActor.angle += 45
-  if key == keys.LEFT:  lastSelectedActor.angle -= 45
+  if key == keys.RIGHT: gs.lastSelectedActor.angle += 45
+  if key == keys.LEFT:  gs.lastSelectedActor.angle -= 45
+  if key == keys.ESCAPE: sys.exit(1)
 
 ### end ###
 
