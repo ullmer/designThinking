d5faf06d866d2b570b9eb6cf0080f706a07e85b2
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-05 18:34:50 -0400

    progress

--- 2024/hcc/readings/exPg8.py
+++ 2024/hcc/readings/exPg8.py
@@ -15,11 +15,13 @@ def readingsPg(Readings):
   dx, dy     = 350, 100
   x0, y0     =  50,  10
   actors     = None
+  actor2id   = None
   numRd      = None
 
-  font1  = "oswald-medium"
-  cwhite = "#ffffff"
-  cblack = "#000000"
+  font1    = "oswald-medium"
+  fontSize = 40
+  cwhite   = "#ffffff"
+  cblack   = "#000000"
 
   actorBgFn  = 'readings_box_1c'
 
@@ -27,7 +29,8 @@ def readingsPg(Readings):
 
   def __init__(self): 
     super().__init__()
-    self.actors = []
+    self.actors   = []
+    self.actor2id = {}
 
     self.numRd    = self.size()
     rxc           = self.rows * self.cols
@@ -45,42 +48,48 @@ def readingsPg(Readings):
     for i in range(self.numRd):
       a = Actor(self.actorBgFn, topleft=(x, y))
       self.actors.append(a); y += self.dy; row += 1
+      self.actor2id(a) = i
 
     if row >= self.rows: 
       row = 0; col += 1; y = self.y0; x += self.dx
 
+  ################## draw ##################
 
-def draw(): 
-  screen.clear()
-  for actor in actors: actor.draw()
-  x, y     = x0, y0
-  row, col = 0, 0
+  def draw(self, screen): 
+    for actor in self.actors: actor.draw()
+    x, y     = x0, y0
+    row, col = 0, 0
 
-  for i in range(numRd):
-    r = readings.getReading(i)
-    drawReading(r, x, y)
-    y += dy; row += 1
-    if row >= rows: 
-      row = 0; col += 1; y = y0; x += dx
+    for i in range(self.numRd):
+      r = self.getReading(i)
+      self.drawReading(screen, r, x, y)
+      y += self.dy; row += 1
+      if row >= self.rows: 
+        row = 0; col += 1; y = self.y0; x += self.dx
 
-def on_mouse_down(pos): 
-  if a1.collidepoint(pos): print("Actor 1 was pressed")
-  if a2.collidepoint(pos): print("Actor 2 was pressed")
+  ################## on_mouse_down ##################
+
+  def on_mouse_down(self.pos): 
+    for i in range(self.numRd):
+      actor = self.actors[i]
+      if actor.collidepoint(pos): print("Actor was pressed:", i)
+
+  ################## draw reading ################## 
   
-def drawReading(reading, x0, y0):
-  au, yr, abTi, prDa = reading.getFields(['author', 'year', 'abbrevTitle', 'presentedDate']) 
-  mo, da = prDa.split('-')
-  fs = 40
-
-  if type(au) is list: au2 = ', '.join(au)
-  else:                au2 = str(au)
-
-  yr2 = str(yr)
-
-  screen.draw.text(au2,  topleft  = (x0+  3, y0- 7), fontsize=fs, fontname=font1, color=cwhite, alpha=0.2)
-  screen.draw.text(yr2,  topright = (x0+285, y0- 7), fontsize=fs, fontname=font1, color=cwhite, alpha=0.2)
-  screen.draw.text(abTi, topleft  = (x0+  3, y0+41), fontsize=fs, fontname=font1, color=cwhite, alpha=0.5)
-  screen.draw.text(mo,   topright = (x0+332, y0- 7), fontsize=fs, fontname=font1, color=cblack, alpha=0.4)
-  screen.draw.text(da,   topright = (x0+332, y0+41), fontsize=fs, fontname=font1, color=cwhite, alpha=0.3)
+  def drawReading(self, screen, reading, x0, y0):
+    au, yr, abTi, prDa = reading.getFields(['author', 'year', 'abbrevTitle', 'presentedDate']) 
+    mo, da = prDa.split('-')
+  
+    if type(au) is list: au2 = ', '.join(au)
+    else:                au2 = str(au)
+  
+    yr2, fs = str(yr), self.fontSize
+    x0, y0  = self.x0, self.y0
+  
+    screen.draw.text(au2,  topleft  = (x0+  3, y0- 7), fontsize=fs, fontname=font1, color=cwhite, alpha=0.2)
+    screen.draw.text(yr2,  topright = (x0+285, y0- 7), fontsize=fs, fontname=font1, color=cwhite, alpha=0.2)
+    screen.draw.text(abTi, topleft  = (x0+  3, y0+41), fontsize=fs, fontname=font1, color=cwhite, alpha=0.5)
+    screen.draw.text(mo,   topright = (x0+332, y0- 7), fontsize=fs, fontname=font1, color=cblack, alpha=0.4)
+    screen.draw.text(da,   topright = (x0+332, y0+41), fontsize=fs, fontname=font1, color=cwhite, alpha=0.3)
 
 ### end ###
