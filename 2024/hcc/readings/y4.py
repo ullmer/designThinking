6aa8997ad9f41d583129ed50684dc880ac244e00
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-05 13:16:22 -0400

    working toward class

---
+++ 2024/hcc/readings/y4.py
@@ -0,0 +1,48 @@
+# Example parsing class reading list
+# Brygg Ullmer, Clemson University
+# Begun 2024-09-05
+
+import yaml
+
+################## Reading class ##################
+
+class Reading: #not catching any errors; caveat emptor
+
+  fields     = ['author', 'year', 'abbrevTitle', 'title', 'studentPresenter']
+  fieldsDict = {}
+
+  def setFieldsFromYaml(self, yd):
+    for field in self.fields: self.fieldsDict[field] = yd[field]
+
+  def setField(self, field, val):      self.fieldsDict[field] = val
+  def getField(self, field):           return self.fieldsDict[field]
+  def printReadingAbbrev(self, field): print(fieldsDict['abbrevTitle'])
+
+################## Readings class ##################
+
+class Readings: #not catching any errors; caveat emptor
+  fn          = 'index.yaml'  #filename
+  yd          = None          #YAML data
+  yc          = None          #YAML extraction for classes
+  readingList = None
+
+  ################## load YAML from named file ##################
+
+  def loadYaml(self, fn): 
+    f       = open(fn, 'rt')
+    self.yd = yaml.safe_load(f)
+    self.yc = self.yd['class'] 
+
+    for classDate in self.yc:
+      classPeriod = self.yc[classDate]
+      for reading in classPeriod:
+        r = Reading()
+        r.setFieldsFromYaml(reading)
+        self.readingList.append(r)
+
+  ################## print reading abbreviations ##################
+
+  def printReadingAbbrevs(self): 
+    for r in self.readingList: r.printReadingAbbrev()
+
+### end ###
