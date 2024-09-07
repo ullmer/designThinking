97ead6295556a20406d24f0a13fa49d744946ccd
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-06 22:48:08 -0400

    progress

--- 2024/hcc/readings/exAnimTranspBox2.py
+++ 2024/hcc/readings/exAnimTranspBox2.py
@@ -16,15 +16,22 @@ TL2 = ( 10,  10); BR2 = (20, 20)
 TL3 = (  5,   5); BR3 = (300, 300)
 TL4 = (500, 500); BR4 = (600, 600)
 
+TL5 = (  5, 700); BR5 = ( 55, 750)
+TL6 = (750, 700); BR6 = (800, 750)
+
 aSrc1, aDest1 = (TL1, BR1), (TL2, BR2)
 aSrc2, aDest2 = (TL3, BR3), (TL4, BR4)
+aSrc3, aDest3 = (TL5, BR5), (TL6, BR6)
 
 etsc = enoTranspSurfaceCache()
 
 #eatb1 = enoAnimTranspBox(topLeft=TL1, bottomRight=BR1)
-eatb1 = enoAnimTranspBox(animSrc=aSrc1, animDest=aDest1, animBounce=True, eTranspSurfaceCache=etsc, animDuration=1.5)
-eatb2 = enoAnimTranspBox(animSrc=aSrc2, animDest=aDest2, animBounce=True, eTranspSurfaceCache=etsc, animDuration=1.5)
+eatb1 = enoAnimTranspBox(animSrc=aSrc1, animDest=aDest1, animBounce=True, eTranspSurfaceCache=etsc, animDuration=4)
+eatb2 = enoAnimTranspBox(animSrc=aSrc2, animDest=aDest2, animBounce=True, eTranspSurfaceCache=etsc, animDuration=4)
 
-def draw(): screen.clear(); eatb1.draw(screen); eatb2.draw(screen)
+cursorColor = (200, 0, 0, 75)
+cursor = enoAnimTranspBox(animSrc=aSrc3, animDest=aDest3, animBounce=True, animDuration=5, lineColor=cursorColor)
+
+def draw(): screen.clear(); eatb1.draw(screen); eatb2.draw(screen); cursor.draw(screen)
 
 ### end ###
