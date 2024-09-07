5b7749752d2ba3b197dddadca274fb4bd5a2793d
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-07 16:35:32 -0400

    iterating toward shadows in freecad

---
+++ 2024/hcc/c3d/exC3dShadow02b.py
@@ -0,0 +1,31 @@
+#https://forum.freecad.org/viewtopic.php?style=5&t=9663&start=30
+
+from pivy import coin
+
+sotype=coin.SoType.fromName("SoShadowGroup")
+shadow=sotype.createInstance()
+shadow.precision.setValue(1)
+shadow.quality.setValue(1)
+
+sotype=coin.SoType.fromName("ShadowDirectionalLight")
+light=sotype.createInstance()
+light.intensity.setValue(200)
+light.direction.setValue(-1,-1,-1)
+
+view=Gui.ActiveDocument.ActiveView.getViewer()
+root=view.getSceneGraph()
+# get the third node of the root (or search for SoSeparator)
+scene=root.getChild(2)
+root.removeChild(scene)
+
+shadow.addChild(light)
+shadow.addChild(scene)
+root.addChild(shadow)
+
+
+c1, c2 = coin.Cube(), coin.Cube()
+c2.width = 3; c2.height=.5
+
+shadow.addChild(c1)
+shadow.addChild(c2)
+
