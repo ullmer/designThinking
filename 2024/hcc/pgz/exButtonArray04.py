47f999ac6b6299abfa2bbc6ad4b08ec6072f630e
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-09 14:34:36 -0400

    progress

--- 2024/hcc/pgz/exButtonArray04.py
+++ 2024/hcc/pgz/exButtonArray04.py
@@ -17,8 +17,8 @@ bd2Labels = ['action', 'store', 'load']
 bd3Labels = ['slot'] + list(range(1,10))
 
 eba1 = enoButtonArray(buttonDim=bd, dx=bdx, labelArray=bd1Labels, basePos=bp1)
-eba2 = enoButtonArray(buttonDim=bd, dx=bdx, labelArray=bd2Labels, basePos=bp2)
-eba3 = enoButtonArray(buttonDim=bd, dx=bdx, labelArray=bd3Labels, basePos=bp3)
+eba2 = enoButtonArray(buttonDim=bd, dx=bdx, labelArray=bd2Labels, basePos=bp2, maxOneToggledOn=True)
+eba3 = enoButtonArray(buttonDim=bd, dx=bdx, labelArray=bd3Labels, basePos=bp3, maxOneToggledOn=True)
 
 headerColor = (50, 50, 50)
 for ba in [eba1, eba2, eba3]: ba.getButtonIdx(0).bgcolor1 = headerColor
@@ -33,7 +33,12 @@ def on_mouse_down(pos):
   for touchable in drawables: touchable.on_mouse_down(pos)
 
 def on_key_down(key): #initially hardwired; sigh
-   if key == 's': eba2.toggleButtonIdx(1) 
-   if key == 'l': eba2.toggleButtonIdx(2)
+   if key == keys.S:   eba2.toggleButtonIdx(1); print('store mode') 
+   if key == keys.L:   eba2.toggleButtonIdx(2); print('load  mode')
+
+   if key.name.startswith('K_') and key.name[2].isdigit():
+     print("num key pressed", key.name[2])
+
+   #if key == keys.K_0: print('0 pressed')
 
 ### end ###
