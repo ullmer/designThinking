3a55d75983b844c76f59fc7fd65eecdd5b7ac7b3
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-10 18:02:13 -0400

    progress

--- 2024/hcc/pgz/enoKbdMgr.py
+++ 2024/hcc/pgz/enoKbdMgr.py
@@ -18,54 +18,60 @@ class enoKbdMgr:
   
     self.keyCallbacksDict = {} #dictionary
 
- ############### register key callback ############### 
+  def err(self, msg): print("Reading error:", msg); traceback.print_exc()
 
- def registerKeyCallback(self, whichKey, callback): #maintain in a list, as multiple callbacks may be present
-   if self.keyCallbacksDict is None: self.err("registerKeyCallback: keyCallbacksDict is unset, should not happen;" return None
-   if whichKey in self.keyCallbacksDict: self.keyCallbacksDict[whichKey].append(callback)
-   else:                               self.keyCallbacksDict[whichKey] = [callback]
+  ############### register key callback ############### 
 
- ############### register key callback ############### 
+  def registerKeyCallback(self, whichKey, callback): #maintain in a list, as multiple callbacks may be present
+    if self.keyCallbacksDict is None: self.err("registerKeyCallback: keyCallbacksDict is unset, should not happen;" return None
+    if whichKey in self.keyCallbacksDict: self.keyCallbacksDict[whichKey].append(callback)
+    else:                               self.keyCallbacksDict[whichKey] = [callback]
 
- def registerKeyCallbacks(self, whichKey, callbackList): 
-   if self.keyCallbacksDict is None: self.err("registerKeyCallbacks: keyCallbacksDict is unset, should not happen;" return None
+  ############### register key callback ############### 
 
-   if isinstance(callbackList, list):
-     for callback in callbackList: self.registerKeyCallback(whichKey, callback) # a bit inefficient, but possible later benefits
-   else: 
-     self.err("registerKeyCallbacks: expecting a callback List, but not present"); return
+  def registerKeyCallbacks(self, whichKey, callbackList): 
+    if self.keyCallbacksDict is None: self.err("registerKeyCallbacks: keyCallbacksDict is unset, should not happen;" return None
 
- ############### trigger key callback ############### 
+    if isinstance(callbackList, list):
+      for callback in callbackList: self.registerKeyCallback(whichKey, callback) # a bit inefficient, but possible later benefits
+    else: 
+      self.err("registerKeyCallbacks: expecting a callback List, but not present"); return
 
- def triggerKeyCallbacks(self, whichKey): 
-   if self.keyCallbacksDict is None: self.err("triggerKeyCallback: keyCallbacksDict is unset, should not happen;" return None
+  ############### trigger key callback ############### 
+ 
+  def triggerKeyCallbacks(self, whichKey): 
+    if self.keyCallbacksDict is None: self.err("triggerKeyCallback: keyCallbacksDict is unset, should not happen;" return None
 
-   if whichKey not in self.keyCallbacksDict: self.err("triggerKeyCallbacks: unregistered key invoked:" + whichKey); returna
+    if whichKey not in self.keyCallbacksDict: self.err("triggerKeyCallbacks: unregistered key invoked:" + whichKey); returna
 
-   for callback in self.keyCallbacksDict:
-     try:    callback()
-     except: self.err("triggerKeyCallbacks: attempted callback, error received"); traceback.print_exc() 
+    for callback in self.keyCallbacksDict:
+      if isinstance(callback, string): 
+        msgStr = "triggerKeyCallbacks: key: %s, val: %s" % (whichKey, callback)
+        self.msg(msgStr); 
+      else:
+        try:    callback()
+        except: self.err("triggerKeyCallbacks: attempted callback, error received"); traceback.print_exc() 
 
- ############### get bound keys ############### 
+  ############### get bound keys ############### 
 
- def getBoundKeys(self): 
-   if self.keyCallbacksDict is None: return None
-   result = list(self.keyCallbacksDict.keys())
-   return result
+  def getBoundKeys(self): 
+    if self.keyCallbacksDict is None: return None
+    result = list(self.keyCallbacksDict.keys())
+    return result
 
- ############### on key down ############### 
+  ############### on key down ############### 
 
- def on_key_down(self, key): 
-   if key in self.keyCallbacksDict:
-   if key == keys.S:   eba2.toggleButtonIdx(1); print('store mode')
-   if key == keys.L:   eba2.toggleButtonIdx(2); print('load  mode')
+  def on_key_down(self, key): 
+    if key in self.keyCallbacksDict:
+    if key == keys.S:   eba2.toggleButtonIdx(1); print('store mode')
+    if key == keys.L:   eba2.toggleButtonIdx(2); print('load  mode')
 
-   if key.name.startswith('K_') and key.name[2].isdigit():
-     digit = ord(key.name[2]) - ord('0')
-     eba3.toggleButtonIdx(digit)
+    if key.name.startswith('K_') and key.name[2].isdigit():
+      digit = ord(key.name[2]) - ord('0')
+      eba3.toggleButtonIdx(digit)
 
- ############### on key up ############### 
+  ############### on key up ############### 
 
- def on_key_up(self, key): pass
+  def on_key_up(self, key): pass
 
 ### end ###
