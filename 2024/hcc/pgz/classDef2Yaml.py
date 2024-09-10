deb51860c397244b667f9084a965c465e9a6c421
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-10 18:12:57 -0400

    progress

--- 2024/hcc/pgz/classDef2Yaml.py
+++ 2024/hcc/pgz/classDef2Yaml.py
@@ -2,12 +2,25 @@
 # Brygg Ullmer, Clemson University
 # Begun 2024-09-10
 
-import glob, sys 
+import sys 
 
 arglen = len(sys.argv)
 
 if arglen < 2: 
   print("classDef2Yaml requires one or more python filenames as arguments"); sys.exit(-1)
+
+filenames = sys.argv[1:]
+
+for fn in filenames:
+  try:
+    f        = open(fn, 'rt')
+    rawlines = f.readlines()
+    print("fn:", fn)
+    for rawline in rawlines:
+      if rawline.find('class ') >= 0 or rawline.find('def: ') >= 0:
+        print(rawline, endline='')
+    f.close()
+  except: pass
 
 ### end ###
 
