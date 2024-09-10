bb42f3186a1655a87f2604d6807fe64127ddc58e
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-10 18:08:44 -0400

    progress

--- 2024/hcc/pgz/classDef2Yaml.py
+++ 2024/hcc/pgz/classDef2Yaml.py
@@ -2,6 +2,12 @@
 # Brygg Ullmer, Clemson University
 # Begun 2024-09-10
 
+import glob, sys 
+
+arglen = len(sys.argv)
+
+if arglen < 2: 
+  print("classDef2Yaml requires one or more python filenames as arguments"); sys.exit(-1)
 
 ### end ###
 
