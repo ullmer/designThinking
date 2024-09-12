aa5933f76a2de2ca0695e78278b6034d1f11ab5a
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-12 10:34:42 -0400

    early victory

--- 2024/hcc/tki/exProjCats1.py
+++ 2024/hcc/tki/exProjCats1.py
@@ -6,15 +6,19 @@ yf  = open(yfn, 'rt')
 yd  = yaml.safe_load(yf)
 print(yd)
 
+def getCategories(yamlData):
+  result = list(yamlData.keys())
+  return result
+
 def helloCB():
   print("hello was pushed")
 
 root      = Tk() 
 
-categories = [1,2,3]
+categories = getCategories(yd)
 
 for category in categories:
-  w    = Button(root, text=category, command=helloCB)
+  w    = Button(root, text=category, command=helloCB, width=15)
   w.pack()
 
 root.mainloop()                                          
