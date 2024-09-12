0516af3817d2005b065d8ca9ee3df0d462f97d7d
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-12 10:35:59 -0400

    replication

---
+++ 2024/hcc/tki/exProjCats2.py
@@ -0,0 +1,27 @@
+import yaml
+from tkinter import *
+
+yfn = 'projCats.yaml'
+yf  = open(yfn, 'rt')
+yd  = yaml.safe_load(yf)
+print(yd)
+
+def getCategories(yamlData):
+  result = list(yamlData.keys())
+  return result
+
+def helloCB():
+  print("hello was pushed")
+
+root      = Tk() 
+
+categories = getCategories(yd)
+
+for category in categories:
+  w    = Button(root, text=category, command=helloCB, width=15)
+  w.pack()
+
+root.mainloop()                                          
+
+# https://en.wikipedia.org/wiki/Tkinter
+### end ###
