70f884b8a49f402349fa73e375c33ace5346662b
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-12 10:28:11 -0400

    progress

--- 2024/hcc/tki/exProjCats1.py
+++ 2024/hcc/tki/exProjCats1.py
@@ -1,7 +1,23 @@
 import yaml
+from tkinter import *
 
 yfn = 'projCats.yaml'
 yf  = open(yfn, 'rt')
 yd  = yaml.safe_load(yf)
 print(yd)
 
+def helloCB():
+  print("hello was pushed")
+
+root      = Tk() 
+
+categories = [1,2,3]
+
+for category in categories:
+  w    = Button(root, text=category, command=helloCB)
+  w.pack()
+
+root.mainloop()                                          
+
+# https://en.wikipedia.org/wiki/Tkinter
+### end ###
