6d32de3c8c88dda58ca9b800d50a30ffdc6afc7b
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-13 08:38:15 -0400

    progress

--- 2024/hcc/tki/genThemesMap.py
+++ 2024/hcc/tki/genThemesMap.py
@@ -8,7 +8,7 @@ lines = sys.stdin.readlines()
 for line in lines:
   nameLetters  = line[:5] #extract the first five letters
   idx          = line.find(": ") #find the ": " as a demarcation
-  themeName    = line[idx:]
-  themeLetters = themeName.strip()[:5]
-  print(nameLetters, ':', themeLetters, ';', end='')
+  themeName    = line[idx+2:].strip()
+  themeLetters = themeName[:10]
+  print(nameLetters + ':' + themeLetters + ';', end='')
 
