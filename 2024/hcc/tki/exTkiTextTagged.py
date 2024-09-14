cde48f0b6bf173108df87076184dba418d13437e
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-14 18:21:31 -0400

    progress

--- 2024/hcc/tki/exTkiTextTagged.py
+++ 2024/hcc/tki/exTkiTextTagged.py
@@ -7,16 +7,32 @@ import lorem
 
 #https://pypi.org/project/lorem/
 
-p = lorem.paragraph()
+lp      = lorem.paragraph()
+lpWords = lp.split()
+lpRows  = []
+
+blockSize = 5
+idx       = 0
+
+for i in range(blockSize): #blockSize lines of blockSize words
+  lpRow = []
+  for j in range(blockSize):
+    lpRow.append(lpWords[idx])
+    idx += 1
+  lpRows.append(lpRow)
+  lpRow = []
 
 root = Tk()              
+
 t = Text(root, width=400) 
 t.pack(expand=True, fill=BOTH)    
 
 t.tag_configure('bold', font=('Calibri', 13, 'bold'))
 t.tag_configure('norm', font=('Calibri', 11))
 
-for i in range(20):
+for lpRow in lpRows:
+  word1    = lpRow[0]
+  wordRest = ' '.join(lpRow[1:]) # a bit inefficient computationally, but...
 
 .insert("1.0",   "name: ")
 .insert(END,   name + "\n")
