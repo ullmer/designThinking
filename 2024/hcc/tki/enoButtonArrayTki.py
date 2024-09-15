6e44bf93deee930a10194a92b3688f3edd7558e8
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-14 22:22:52 -0400

    progress

--- 2024/hcc/tki/enoButtonArrayTki.py
+++ 2024/hcc/tki/enoButtonArrayTki.py
@@ -1,2 +1,48 @@
-projCats.yaml
-themeFields.yaml
+# Button field array, with keystroke shortcuts, toggleable
+# Brygg Ullmer, Clemson University
+# Begun 2024-09-14
+
+import tkinter as tk
+
+class enoButtonArrayTki:
+  yamlFieldDescriptorsFn = 'themeFields.yaml'
+  yamlFieldDescriptorsD  = None
+
+  root         = None
+  buttonState  = {}
+  buttonTk     = {}
+  hideTitlebar = False
+
+  ############# constructor #############
+
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+
+    self.buildUI()
+
+  #################### build user interface ####################
+
+  def buildUI(self):
+  
+    self.root = tk.Tk()
+
+    #self.root.title("Interactive grid example")
+    #self.root.geometry(self.windowGeometry)
+
+    if self.hideTitlebar: self.root.overrideredirect(1) #hide window decorations ~= titlebar
+  
+    self.buttonState = {}
+    self.buttonTk    = {}
+
+  ############### button toggle callback ############### 
+  
+  def toggleCB(self, coord):
+    if self.buttonState[coord]: 
+      self.buttonState[coord] = False
+      print("toggleCB on %s: off" % str(coord))
+
+  ################# load yaml ################# 
+
+  def loadYaml(self):
+
+### end ###
