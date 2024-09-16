e5df004550111f79a922157c6bb4bc220b953739
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-09-15 23:48:52 -0400

    replicating exProjCats6, about to extend

---
+++ 2024/hcc/tki/exProjCats7.py
@@ -0,0 +1,42 @@
+# In-class example in HCC Fundamentals
+# Brygg Ullmer, Clemson University
+# Begun 2024-09-12
+
+from tkinter             import *
+from tkinter.font        import *
+from hccStudentThemesTki import *
+from functools           import partial
+
+root = Tk() 
+cw   = 25 #column width
+
+root.title("HCC student themes navigator")
+
+try:    headerFont = Font(family="Calibri", size=15, weight=BOLD)
+except: headerFont = ('Sans','15','bold') #fallback if Calibri not installed
+
+try:    bodyFont = Font(family="Calibri", size=13)
+except: bodyFont = ('Sans','13') 
+
+st          = studentThemesTki()
+categories  = st.getCategories()
+
+for category in categories:
+  f    = Frame(root); f.pack(side=LEFT, anchor="n") #anchor to the north
+  b    = Button(f, text=category, font=headerFont, bg='#aaa')
+  b.pack(expand=True, fill=BOTH)
+
+  subthemes = st.getCatEntries(category)
+  for subtheme in subthemes:
+    cb = partial(st.displayStudentTheme, subtheme) #callback to display associated information
+    b2 = Button(f, text=subtheme, width=cw, anchor="w", font=bodyFont, command=cb) #anchor to the west means left-aligned
+    b2.pack(side=TOP)
+
+studentKeys      = st.getStudentKeys()
+firstStudent     = studentKeys[0]
+studentViewFrame = st.buildStudentThemeView(root, firstStudent)
+studentViewFrame.pack(expand=True, fill=BOTH)
+
+root.mainloop()                                          
+
+### end ###
