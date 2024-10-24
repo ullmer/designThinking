feb03fd65e15e4bf6a4552ee2e98fe694ad0958d
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-24 12:04:27 -0400

    iterating

--- 2024/hcc/solidPython/exSolid03b.py
+++ 2024/hcc/solidPython/exSolid03b.py
@@ -4,20 +4,23 @@
 
 from solid import *        # load in SolidPython/SCAD support code
 
-cyl1  = cylinder(r=.5,  h=1)
-cyl2  = cylinder(r=.4,  h=1.5)
-cyl3  = cylinder(r=.45, h=2)
+cyl1a = cylinder(r=.5,  h=1.5)
+cyl2a = cylinder(r=.4,  h=2)
+cyl3a = cylinder(r=.45, h=2)
 
-cyl1b = scale([1.5, 1, 1])(    cyl1)
-cyl2b = scale([1.5, 1, 1])(    cyl2)
-cyl3a = rotate(a = [90, 0, 0])(cyl3)
+cyl1b = scale([1.5, 1, 1])(    cyl1a)
+cyl2b = scale([1.5, 1, 1])(    cyl2a)
+cyl3a = rotate(a = [90, 0, 0])(cyl3a)
 cyl3b = translate([0, 0.7, .5])(  cyl3a)
 
-cub1  = cube()
-cub1a = translate([-.5, -.5, 0])(cub1)
-cub1b = scale([2, .2, 2])(cub1a)
+cube1a = cube()
+cube1b = translate([-.5, -.5, 0])(cube1a)
+cube1c = scale([2, .2, 2])(cube1b)
 
-outGeom = cyl1b - cyl2b - cub1b + cyl3b
+cube2a = cube(2)
+cube2b = translate([-1, -1, 1])(cube2a)
+
+outGeom = cyl1b - cyl2b - cube1c + cyl3b + cube2b
 
 radialSegments = 90; hdr = '$fn = %s;' % radialSegments # create a header for the export
 scad_render_to_file(outGeom, 'exSolid03b.scad', file_header=hdr) # write the .scad file
