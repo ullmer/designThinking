82296efef8e540135770e3e4aefeead684e15713
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-24 12:35:46 -0400

    progress

--- 2024/hcc/solidPython/exSolid03c.py
+++ 2024/hcc/solidPython/exSolid03c.py
@@ -15,23 +15,25 @@ cyl2b = scale([1.5, 1, 1])(    cyl2a)
 cyl3b = rotate(a = [90, 0, 0])(cyl3a)
 cyl4b = rotate(a = [90, 0, 0])(cyl4a)
 
-cyl3c = translate([  0, .7, .5])(cyl3b)
-cyl4c = translate([-.25, .7, .7])(cyl4b)
+cyl3c = translate([  0,  .7, .5])(cyl3b)
+cyl4c = translate([-.25, .7, .59])(cyl4b)
 
 cube1a = cube()
 cube1b = translate([-.5, -.5, 0])(cube1a)
 cube1c = scale([2, .2, 2])(       cube1b)
 
 cube2a = cube(2)
-cube2b = translate([-1.3, -.8, 1])(cube2a)
+cube2b = translate([-1.4, -.8, 1])(cube2a)
 cube2c = rotate(a =[0, 15, 0])(    cube2b)
 
 cube3a = cube(1.5)
-cube3b = translate([-1.5, -.8, .7])(cube3a)
+cube3b = translate([-1.65, -.8, .7])(cube3a)
 
 #outGeom = cyl1b - cyl2b - cube1c + cyl3b + cube2c
 #outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c
-outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c + cyl4c + cube3b
+#outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c + cube3b + cyl4c
+#outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c + (cube3b - cyl4c)
+outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c - (cube3b - cyl4c)
 
 radialSegments = 90; hdr = '$fn = %s;' % radialSegments # create a header for the export
 scad_render_to_file(outGeom, 'exSolid03b.scad', file_header=hdr) # write the .scad file
