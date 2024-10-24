2b7f996ef31a1d434de095987b7eef0ea14d6a24
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-24 11:59:54 -0400

    adding side-cylinder

--- 2024/hcc/solidPython/exSolid03b.py
+++ 2024/hcc/solidPython/exSolid03b.py
@@ -4,21 +4,23 @@
 
 from solid import *        # load in SolidPython/SCAD support code
 
-#c1 = cylinder(r=.3, h=.6)
-cy1  = cylinder(r=.5, h=.5)
-cy1b = scale([1.5, 1, 1])(cy1)
+cyl1  = cylinder(r=.5,  h=1)
+cyl2  = cylinder(r=.4,  h=1.5)
+cyl3  = cylinder(r=.45, h=2)
 
-cy2  = cylinder(r=.4, h=.6)
-cy2b = scale([1.5, 1, 1])(cy2)
+cyl1b = scale([1.5, 1, 1])(    cyl1)
+cyl2b = scale([1.5, 1, 1])(    cyl2)
+cyl3a = rotate(a = [90, 0, 0])(cyl3)
+cyl3b = translate([0, 0.7, .5])(  cyl3a)
 
-cu1  = cube()
-cu1a = translate([-.5, -.5, 0])(cu1)
-cu1b = scale([2, .2, 2])(cu1a)
+cub1  = cube()
+cub1a = translate([-.5, -.5, 0])(cub1)
+cub1b = scale([2, .2, 2])(cub1a)
 
-outGeom = cy1b - cy2b - cu1b
+outGeom = cyl1b - cyl2b - cub1b + cyl3b
 
-radialSegments = 25; hdr = '$fn = %s;' % radialSegments # create a header for the export
-scad_render_to_file(outGeom, 'exSolid03a.scad', file_header=hdr) # write the .scad file
+radialSegments = 90; hdr = '$fn = %s;' % radialSegments # create a header for the export
+scad_render_to_file(outGeom, 'exSolid03b.scad', file_header=hdr) # write the .scad file
 
 ### end ###
 
