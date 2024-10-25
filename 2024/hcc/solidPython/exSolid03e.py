5cc9a63e4f308aaf96e9d3ba27676b71603ccb94
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-25 09:39:22 -0400

    code alignments

--- 2024/hcc/solidPython/exSolid03e.py
+++ 2024/hcc/solidPython/exSolid03e.py
@@ -8,20 +8,17 @@ cyl1a = cylinder(r=.5, h=1.5); cyl1b = scale([1.5, 1, 1])(cyl1a)
 cyl2a = cylinder(r=.4, h=2);   cyl2b = scale([1.5, 1, 1])(cyl2a)
 cyl3a = cylinder(r=.45,h=2);   cyl3b = rotate(a = [90, 0, 0])(cyl3a)
 cyl4a = cylinder(r=.5, h=2);   cyl4b = rotate(a = [90, 0, 0])(cyl4a)
-cyl5a = cylinder(r=.65, h=2);   cyl5b = rotate(a = [90, 0, 0])(cyl5a)
-#cyl5a = cylinder(r=.5, h=2);   cyl5b = rotate(a = [90, 0, 0])(cyl5a)
+cyl5a = cylinder(r=.65, h=2);  cyl5b = rotate(a = [90, 0, 0])(cyl5a)
 
 cyl3c = translate([  0,  .7, .5 ])(cyl3b)
 cyl4c = translate([-.25, .7, .59])(cyl4b)
-#cyl5c = translate([.29, .7, .46])(cyl4b)
-cyl5c = translate([ .09, .7, .4])(cyl5b)
+cyl5c = translate([ .09, .7, .4 ])(cyl5b)
 
 cube1a, cube2a, cube3a, cube4a = cube(), cube(2), cube(1.5), cube(1.5)
 
-cube1b = translate([ -.5,  -.5, 0 ])(cube1a); cube1c = scale(    [2, .2, 2])(cube1b)
-cube2b = translate([-1.4,  -.8, 1 ])(cube2a); cube2c = rotate(a =[0, 15, 0])(cube2b)
-cube3b = translate([-1.65, -.8, .7])(cube3a)
-#cube4b = translate([  .5, -.8, .7])(cube4a)
+cube1b = translate([ -.5, -.5, 0 ])(cube1a); cube1c = scale(    [2, .2, 2])(cube1b)
+cube2b = translate([-1.4, -.8, 1 ])(cube2a); cube2c = rotate(a =[0, 15, 0])(cube2b)
+cube3b = translate([-1.65,-.8, .7])(cube3a)
 cube4b = translate([  .5, -.8, .5])(cube4a)
 
 #outGeom = cyl1b - cyl2b - cube1c + cyl3b + cube2c
@@ -34,7 +31,7 @@ outGeom -= (cube3b - cyl4c)
 outGeom -= (cube4b - cyl5c)
 
 radialSegments = 90; hdr = '$fn = %s;' % radialSegments # create a header for the export
-scad_render_to_file(outGeom, 'exSolid03d.scad', file_header=hdr) # write the .scad file
+scad_render_to_file(outGeom, 'exSolid03e.scad', file_header=hdr) # write the .scad file
 
 ### end ###
 
