31a64156951aea823621e357fca55c7826fca34f
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-25 09:04:38 -0400

    compacting code, about to integrate one more re-shape

---
+++ 2024/hcc/solidPython/exSolid03d.py
@@ -0,0 +1,31 @@
+# SolidPython example code 
+# Brygg Ullmer, Clemson University
+# Written 2024-10-24
+
+from solid import *        # load in SolidPython/SCAD support code
+
+cyl1a = cylinder(r=.5, h=1.5); cyl1b = scale([1.5, 1, 1])(cyl1a)
+cyl2a = cylinder(r=.4, h=2);   cyl2b = scale([1.5, 1, 1])(cyl2a)
+cyl3a = cylinder(r=.45,h=2);   cyl3b = rotate(a = [90, 0, 0])(cyl3a)
+cyl4a = cylinder(r=.5, h=2);   cyl4b = rotate(a = [90, 0, 0])(cyl4a)
+
+cyl3c = translate([  0,  .7, .5])(cyl3b)
+cyl4c = translate([-.25, .7, .59])(cyl4b)
+
+cube1a = cube(); cube2a = cube(2); cube3a = cube(1.5)
+
+cube1b = translate([ -.5,  -.5, 0 ])(cube1a); cube1c = scale(    [2, .2, 2])(cube1b)
+cube2b = translate([-1.4,  -.8, 1 ])(cube2a); cube2c = rotate(a =[0, 15, 0])(cube2b)
+cube3b = translate([-1.65, -.8, .7])(cube3a)
+
+#outGeom = cyl1b - cyl2b - cube1c + cyl3b + cube2c
+#outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c
+#outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c + cube3b + cyl4c
+#outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c + (cube3b - cyl4c)
+outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c - (cube3b - cyl4c)
+
+radialSegments = 90; hdr = '$fn = %s;' % radialSegments # create a header for the export
+scad_render_to_file(outGeom, 'exSolid03d.scad', file_header=hdr) # write the .scad file
+
+### end ###
+
