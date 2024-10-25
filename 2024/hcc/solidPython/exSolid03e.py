a21e6466090545d537c2261a4e5b6efeba619dc3
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-10-25 09:50:11 -0400

    penetrations as first envisioned, with explicit functional implications; a few more excisions awaiting there

--- 2024/hcc/solidPython/exSolid03e.py
+++ 2024/hcc/solidPython/exSolid03e.py
@@ -14,9 +14,14 @@ cyl6a = cylinder(r=.1,  h=2);   cyl6b = rotate(a = [90, 0, 0])(cyl6a)
 cyl3c  = translate([  0,   .7, .5 ])(cyl3b)
 cyl4c  = translate([-.25,  .7, .59])(cyl4b)
 cyl5c  = translate([ .09,  .7, .4 ])(cyl5b)
+
 cyl6c1 = translate([ .575, .7, .15])(cyl6b)
 cyl6c2 = translate([  0,   0, .225])(cyl6c1)
 cyl6c3 = translate([  0,   0, .225])(cyl6c2)
+
+cyl7c1 = translate([-.575, .7, .15])(cyl6b)
+cyl7c2 = translate([  0,   0, .225])(cyl7c1)
+cyl7c3 = translate([  0,   0, .225])(cyl7c2)
 
 cube1a, cube2a, cube3a, cube4a = cube(), cube(2), cube(1.5), cube(1.5)
 
@@ -33,7 +38,7 @@ cube4b = translate([  .5, -.8, .5])(cube4a)
 outGeom = cyl1b - cyl2b - cube1c - cyl3c - cube2c
 outGeom -= (cube3b - cyl4c) 
 outGeom -= (cube4b - cyl5c)
-outGeom += (cyl6c1 + cyl6c2 + cyl6c3)
+outGeom -= (cyl6c1 + cyl6c2 + cyl6c3) + (cyl7c1 + cyl7c2 + cyl7c3)
 
 radialSegments = 90; hdr = '$fn = %s;' % radialSegments # create a header for the export
 scad_render_to_file(outGeom, 'exSolid03e.scad', file_header=hdr) # write the .scad file
