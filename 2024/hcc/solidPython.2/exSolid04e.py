10453b7a407b2d5c4096d3d4e9099e014fe0fa72
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2024-12-04 16:29:25 -0500

    added a few more comments

--- 2024/hcc/solidPython.2/exSolid04e.py
+++ 2024/hcc/solidPython.2/exSolid04e.py
@@ -5,9 +5,13 @@
 
 from solid import *        # load in SolidPython/SCAD support code
 
+#First, create the main body (an elliptical pipe)
+
 cyl1a = cylinder(r=.5,  h=1.5); cyl1b = scale([1.5, 1,   1])(cyl1a) #cyl1b expresses the outer perimeter.  
 cyl2a = cylinder(r=.4,  h=2);   cyl2b = scale([1.6, 1.1, 1])(cyl2a) #cyl2b is for cutting away the inside
 outGeom = cyl1b - cyl2b #output as exSolid04a.png. this performs the subtraction/cut, leaving an elliptical pipe.  
+
+#Next, cleave it in two halves, and perforate it horizontally
 
 cube1a = cube();  cube1b = translate([ -.5, -.5, 0 ])(cube1a); cube1c = scale([2, .2, 2])(cube1b) #for cutting the pipe in two
 cyl3a  = cylinder(r=.45, h=2); cyl3b = rotate(a = [90, 0, 0])(cyl3a)
@@ -15,6 +19,8 @@ cyl3c  = translate([0, .7, .5 ])(cyl3b) #cylinder for large hole perforating the
 
 outGeom -= cube1c       #cleave the pipe into left and right halves
 outGeom -= cyl3c        #output as exSolid04b.png. perforate the two halves of the oculus 
+
+#Then, trim the top at an angle, and round out the front and back (a "bevel/fillet," without the benefit of that function)
 
 trimTopHeight = 1; trimTopAngle = 15 #15 degree angle to trimming of top of oculus
 cube2a = cube(2); cube2b = translate([-1.4, -.8, trimTopHeight])(cube2a); cube2c = rotate(a = [0, trimTopAngle, 0])(cube2b)
@@ -29,6 +35,19 @@ cyl5a  = cylinder(r=.65, h=2); cyl5b = rotate(a = [90, 0, 0])(cyl5a); cyl5c  = t
 # SolidPython doesn't presently provide a bevel/fillet, but we can approximate
 roundOutTopBack  = cube3b - cyl4c; outGeom -= roundOutTopBack 
 roundOutTopFront = cube4b - cyl5c; outGeom -= roundOutTopFront #output as exSolid04d.png (angle-trimmed top, rounded out top ends)
+
+#Next, 
+cyl6a = cylinder(r=.1,  h=2); cyl6b = rotate(a = [90, 0, 0])(cyl6a)
+
+
+cyl6c1 = translate([ .575, .7, .15])(cyl6b)
+cyl6c2 = translate([  0,   0, .225])(cyl6c1)
+cyl6c3 = translate([  0,   0, .225])(cyl6c2)
+
+cyl7c1 = translate([-.575, .7, .15])(cyl6b)
+cyl7c2 = translate([  0,   0, .225])(cyl7c1)
+cyl7c3 = translate([  0,   0, .225])(cyl7c2)
+
 
 radialSegments = 90; hdr = '$fn = %s;' % radialSegments # create a header, expressing the number of radial segments
 scad_render_to_file(outGeom, 'exSolid04d.scad', file_header=hdr) # write the .scad file
