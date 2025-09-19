d22485fc7dfde7f6807a8a4108be359dddc0f983
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-09-18 22:44:08 -0400

    tweeks

---
+++ 2025/hcc/tki/exTkSheet03.py
@@ -0,0 +1,17 @@
+# https://www.activestate.com/resources/quick-reads/how-to-display-data-in-a-table-using-tkinter/
+
+import tkinter as tk
+import tksheet
+top = tk.Tk()
+sheet = tksheet.Sheet(top, theme='dark blue')
+sheet.grid()
+sheet.default_column_width(20)
+sheet.set_sheet_data([[f"{ri+cj}" for cj in range(9)] for ri in range(9)])
+# table enable choices listed below:
+sheet.enable_bindings(("single_select", "row_select",    "column_width_resize", "arrowkeys",
+                       "rc_select",     "rc_insert_row", "rc_delete_row",       
+                       "copy", "cut", "paste", "delete", "undo", "edit_cell",
+                       "right_click_popup_menu"))
+top.mainloop()
+
+### end ###
