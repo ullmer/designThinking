61301dccac7d9a066371962edb133dbaf521fef8
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-11-03 21:27:22 -0500

    first steps

--- 2025/hcc/mbrowser.01/enoPrismBars.py
+++ 2025/hcc/mbrowser.01/enoPrismBars.py
@@ -1,15 +1,29 @@
+# Enodia "prism bars"
+# Brygg Ullmer, Clemson University
+# Begun 2025-11-03
+
 import pygame
 
-# Create a transparent surface
-poly1 = pygame.Surface((400, 400), pygame.SRCALPHA)
-poly2 = pygame.Surface((400, 400), pygame.SRCALPHA)
+class EnoPrismBars:
+  pathWidth = 100
+  pathMaxDx = 300
+  pathMaxDy = 
+    
+  def createColors(self, ):
+    # Define a color with alpha (RGBA)
+    colRed = (255, 0, 0, 128)  # Semi-transparent red
 
-# Define polygon points
-points1 = [(100, 100), (300, 100), (350, 300), (150, 350)]
-points2 = [(200,   0), (400, 400), (  0, 400)]
 
-# Define a color with alpha (RGBA)
-colRed = (255, 0, 0, 128)  # Semi-transparent red
+
+  def createSurface(self, ):
+
+    # Create a transparent surface
+    poly1 = pygame.Surface((400, 400), pygame.SRCALPHA)
+    poly2 = pygame.Surface((400, 400), pygame.SRCALPHA)
+
+    # Define polygon points
+    points1 = [(100, 100), (300, 100), (350, 300), (150, 350)]
+    points2 = [(200,   0), (400, 400), (  0, 400)]
 
 # Draw the polygon on the transparent surface
 pygame.draw.polygon(poly1, colRed, points1)
