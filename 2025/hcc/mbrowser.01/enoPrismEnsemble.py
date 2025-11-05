91ea12e35210917dba158266dac2dccd27871ddd
Brygg Ullmer <bullmer@clemson.edu>
2025-11-05 17:53:02 +0000

    replicating *Bars; will fuse integrations from test/e*6*

---
+++ 2025/hcc/mbrowser.01/enoPrismEnsemble.py
@@ -0,0 +1,67 @@
+# Enodia "prism bars"
+# Brygg Ullmer, Clemson University
+# Begun 2025-11-03
+
+import math
+import pygame
+import pygame.gfxdraw
+
+from ataBase     import *
+from enoPrismBar import *
+
+class EnoPrismBars(AtaBase):
+  basePos   = (0, 0)
+  cumPosTop = None
+  cumPosBot = None
+  pathWidth = 500
+  baseWidth = None
+  pathMaxDx = 800
+  pathMaxDy = 850
+
+  fontSize    = 30
+  barList     = None
+  flowLeft    = True
+  textOffset2 = (0,0)
+
+  ############# constructor #############
+
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+
+  ############# addBar #############
+
+  def addBar(self, barText, barColor, barWidth): 
+    if self.barList is None: 
+      self.barList = []
+      self.cumPosTop = self.basePos
+      #self.cumPosBot = self.basePos #TBD; incorrect! 
+
+    bottomWidth = barWidth
+    if self.baseWidth is not None: bottomWidth = self.baseWidth
+
+    fl       = self.flowLeft
+    fs       = self.fontSize
+    to2      = self.textOffset2
+    cx1, cy1 = self.cumPosTop
+    #cx2, cy2 = self.cumPosBot
+    epb    = EnoPrismBar(textStrs=barText,     barColor=barColor, barWidth=barWidth, 
+                         basePos =self.cumPosTop, flowLeft=fl,    textOffset2=to2, 
+                         pathMaxDx=self.pathMaxDx, pathMaxDy=self.pathMaxDy, fontSize=fs,
+                         baseWidth=self.baseWidth) #, botPosXStart=cx2)
+
+    cx1           += barWidth
+    #cx2           += bottomWidth
+    self.cumPosTop = (cx1, cy1)
+    #self.cumPosBot = (cx2, cy2)
+    
+    self.barList.append(epb)
+
+  ############# draw #############
+
+  def draw(self, screen):
+    if self.barList is None:
+      if self.verbose: self.msg("draw called with empty barlist"); return
+
+    for b in self.barList: b.draw(screen)
+
+### end ###
