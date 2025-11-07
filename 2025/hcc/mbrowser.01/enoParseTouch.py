850a535ba7d27eb51f97fcd0077935e10773a824
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-11-06 20:28:01 -0500

    progress

--- 2025/hcc/mbrowser.01/enoParseTouch.py
+++ 2025/hcc/mbrowser.01/enoParseTouch.py
@@ -2,4 +2,13 @@
 # Brygg Ullmer, Clemson University
 # Begun 2025-11-06
 
+import ataBase
+
+class EnoParseTouch(AtaBase):
+
+  ############# constructor #############
+
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+
 ### end ###
