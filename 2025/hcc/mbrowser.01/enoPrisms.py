b7567d5a8ec69409a6d8fc0f8e8fc5e1c694f117
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-11-06 21:31:32 -0500

    progress

---
+++ 2025/hcc/mbrowser.01/enoPrisms.py
@@ -0,0 +1,35 @@
+# Enodia "prism"
+# Brygg Ullmer, Clemson University
+# Begun 2025-11-06
+
+import math
+import pygame
+import pygame.gfxdraw
+
+from ataBase       import *
+from enoPrismBar   import *
+from enoPrismBars  import *
+from enoParseTouch import *
+
+class EnoPrisms(AtaBase):
+  prisms = None
+
+  ############# constructor #############
+
+  def __init__(self, **kwargs):
+    self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+    self.prisms = []
+
+  ############# add prism #############
+
+  def addPrism(self, prism):
+    self.prisms.append(prism)
+
+  ############# draw #############
+
+  def draw(self, screen):
+    try: 
+      for p in self.prisms: p.draw(screen)
+    except: self.err("draw")
+
+### end ###
