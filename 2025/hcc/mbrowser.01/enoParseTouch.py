004f0d2d727778f71e9ee5bc935a9cd05c4b3e00
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-11-06 20:31:44 -0500

    progress

--- 2025/hcc/mbrowser.01/enoParseTouch.py
+++ 2025/hcc/mbrowser.01/enoParseTouch.py
@@ -6,9 +6,29 @@ import ataBase
 
 class EnoParseTouch(AtaBase):
 
+  cols, rows = None, None
+  x0, y0     = None, None
+  pixDim     = None
+  bindings   = None
+
   ############# constructor #############
 
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
+
+  ############# keyFieldsPopulated #############
+
+  def keyFieldUnpopulated(self):
+    try:
+      for el in [self.cols, self.rows, self.x0, self.y0, self.pixDim]:
+        if el is None: return True
+      return  False
+
+    except: self.err(keyFieldsPopulated); return None
+
+  ############# parseLocus #############
+
+  def parseLocus(self, pos):
+    if self.keyFieldUnpopulated(): self.msg("parseLocus: key field unpopulated!"); return None
 
 ### end ###
