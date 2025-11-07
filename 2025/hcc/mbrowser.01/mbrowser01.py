253b4bd98729e0f05e0b747db72ec65baf57dca0
U-CAMPUS\bullmer <bullmer@SOC-BULLM-WR3.CAMPUS.CU.CLEMSON.EDU>
2025-11-06 21:18:46 -0500

    replication of test/exEPB10.py

--- 2025/hcc/mbrowser.01/mbrowser01.py
+++ 2025/hcc/mbrowser.01/mbrowser01.py
@@ -15,72 +15,28 @@ WIDTH, HEIGHT=1600,1080
 from enoPrismBar  import *
 from enoPrismBars import *
 from enoActor     import *
+from prismsTei    import *
 
 ##### main ##### 
 
-cblu = (0,   0, 255, 80)
-cyel = (255, 255, 0, 70)
-cgre = (0,   255, 0, 70)
-cred = (255,   0, 0, 70)
+pt = PrismsTei()
+p0 = pt.summonPrism('teiLandscape', 0)
+p1 = pt.summonPrism('teiYearsQ4',   1)
 
-n,w=35,400
-epb1 = EnoPrismBars(flowLeft=False, textOffset2=(-18,0), fontSize=25, pathMaxDx=900)
-epb1.addBar("22: Daejeon",  cyel, n)
-epb1.addBar("23: Warsaw",   cblu, n)
-epb1.addBar("24: Cork",     cblu, w)
-epb1.addBar("25: Bordeaux", cblu, n)
-epb1.addBar("26: Chicago",  cred, n)
+rb = RefractBar((WIDTH, 80), (0, 750))
 
-epb1b = EnoPrismBars(flowLeft=False, pathMaxDx=140, pathMaxDy=80, baseWidth=78, basePos=(900, 850))
-#epb1b = EnoPrismBars(flowLeft=False, pathMaxDx=140, pathMaxDy=75, basePos=(900, 850))
-epb1b.addBar("", cyel, n)
-#epb1b.addBar("", cblu, n)
-#epb1b.addBar("", cblu, w)
-#epb1b.addBar("", cblu, n)
-#epb1b.addBar("", cred, n)
-
-n2=88
-epb2 = EnoPrismBars(flowLeft=True, textOffset2=(705, 0), fontSize=25)
-epb2.addBar("creativity",    cgre, n2)
-epb2.addBar("dance+theater", cgre, n2)
-epb2.addBar("music+sound",   cgre, n2)
-epb2.addBar("actuation",     cyel, n2)
-epb2.addBar("AI",            cyel, n2)
-epb2.addBar("computing hardware", cyel, n2)
-
-epb2b = EnoPrismBars(flowLeft=True, textOffset2=(705, 0), pathMaxDy=80, pathMaxDx=0,
-                     fontSize=25, basePos=(0, 850)) #, baseWidth=80)
-                     #baseWidth=20, fontSize=25, basePos=(50, 850))
-epb2b.addBar("", cgre, n2)
-
-
-refractBar = pygame.Surface((WIDTH, 75), pygame.SRCALPHAg
-gcolor = (255, 255, 255, 75)
-refractBar.fill(rcolor)
-
-def setup():
-  global bs
-  b1 = EnoActor("teiland04",      bottomleft=(0,  1190))
-  b2 = EnoActor("teiblockconf04", bottomleft=(910,1190))
-  sc = .45
-  b1.scaleV(sc)
-  b2.scaleV(sc)
-  bs = [b1, b2]
-
-initialized = False
-
-def update():
-  global initialized
-  if not initialized: setup(); initialized=True
-
-epb = [epb1, epb1b, epb2, epb2b]
+def update(): pt.update()
 
 def draw(): 
   screen.clear()
-  for p in epb: p.draw(screen)
-  for b in bs:  b.draw(screen)
-  screen.blit(refractBar, (0, 800))
+  pt.draw(screen)
+  rb.draw(screen)
+  rcol = rb.fillColor
+  screen.draw.text("TEI", midleft=(305,70), alpha=.2, color=rcol, fontname="barlow_black", fontsize=250)
+
+#def on_mouse_down(pos):
+#  parsePress(pos)
 
 pgzrun.go()
 
-g## end ###
+### end ###
