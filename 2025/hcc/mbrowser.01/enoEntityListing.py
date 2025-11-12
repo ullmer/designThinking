3db846ab61ae1ab5597783b054aeca29f1071955
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-11-12 18:14:13 -0500

    progress

--- 2025/hcc/mbrowser.01/enoEntityListing.py
+++ 2025/hcc/mbrowser.01/enoEntityListing.py
@@ -7,26 +7,37 @@ from enoPrism      import *
 from enoPrismsTei  import *
 
 class EnoEntityListing(AtaBase):
-  prisms = None
-  domainPrisms = None
+  entries          = None
+  entryFont        = "barlow_condensed_extralight"
+  entryFontSize    = 32
+
+  entryFieldWidths = None
 
   ############# constructor #############
 
   def __init__(self, **kwargs):
     self.__dict__.update(kwargs) #allow class fields to be passed in constructor
-    self.prisms       = []
-    self.domainPrisms = []
+    if self.entries is None: self.entries = []
 
-    ept = EnoPrismsTei() #clearly needs further abstraction; a bridge 
-    self.domainPrisms.append(ept)
+  ############# add entry #############
+
+  def addEntry(self, entry): 
+    if not isinstance(self.entries, list):
+      self.msg("addEntry issue: internal entries not populated with a list!")
+      return False
+
+    try:    self.entries.append(entry)
+    except: self.err("addEntry")
 
   ############# draw #############
 
   def draw(self, screen):
     try: 
-      for  p in self.prisms: p.draw(screen)
-      for dp in self.domainPrisms: dp.draw(screen)
+      if not isinstance(self.entries, list):
+        self.msg("draw issue: internal entries not populated with a list!")
+        return False
 
+      for  entry in self.entries: self.drawEntry(entry)
     except: self.err("draw")
 
 ### end ###
