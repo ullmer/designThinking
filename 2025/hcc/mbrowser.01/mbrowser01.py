eb3a6a43f13df07efd63ff4667b24c2a8ee78120
Brygg Ullmer <bullmer@Bryggs-MacBook-Pro-2.local>
2025-11-12 19:10:57 -0500

    progress

--- 2025/hcc/mbrowser.01/mbrowser01.py
+++ 2025/hcc/mbrowser.01/mbrowser01.py
@@ -2,19 +2,20 @@
 # Brygg Ullmer, Clemson University
 # Begun 2025-11-03
 
-import os,sys
+import os, sys
 os.environ['SDL_VIDEO_WINDOW_POS'] = '0,0' #place window at 0,0 
 sys.path.insert(0, #access module in parent directory (for test stubs)
   os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import pgzrun
+import pgzrun, yaml
 from pgzero.builtins import Actor, animate, keyboard, keys
 
 WIDTH, HEIGHT=1600,1080
 
-from enoPrisms     import *
-from enoRefractBar import *
-from enoFrameBox   import *
+from enoPrisms        import *
+from enoRefractBar    import *
+from enoFrameBox      import *
+from enoEntityListing import *
 
 ##### main ##### 
 
@@ -22,18 +23,27 @@ ep = EnoPrisms()
 ep.summonPrism('teiLandscape', 0)
 ep.summonPrism('teiYearsQ4',   1)
 
+yf  = open('yaml/sampleEntries.yaml', 'rt']
+yd  = yaml.safe_load(yf); yf.close()
+eel = EnoEntityListing(entries=yd)
+
 rb  = EnoRefractBar((WIDTH, 80), (0, 750))
 efb = EnoFrameBox()
 
 def update(): ep.update()
 
+################ draw ################
 def draw(): 
   screen.clear()
   ep.draw(screen)
   rb.draw(screen)
   rcol = rb.fillColor
-  screen.draw.text("TEI", midleft=(305,70), alpha=.2, color=rcol, fontname="barlow_black", fontsize=250)
+  screen.draw.text("TEI", midleft=(305,70), alpha=.2, 
+    color=rcol, fontname="barlow_black", fontsize=250)
   efb.draw(screen)
+  eel.draw(screen)
+
+################ on_mouse/key_down ################
 
 def on_mouse_down(pos):
   ep.parseLocus(pos)
